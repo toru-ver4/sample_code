c331017bdb8e0fc30e3d32dde4bbc36b184347dc
Toru Yoshihara <toru.ver.11@gmail.com>
2019-09-23 15:33:34 +0900

    標準入力受信部分、試作OK。

--- 2019/009_python_subprocess/src/device_control.cpp
+++ 2019/009_python_subprocess/src/device_control.cpp
@@ -1,8 +1,28 @@
 #include "device_control.hpp"
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+
+// define
+#define COMMUNICATE_BUF_MAX_LEN 256
+#define EOF_STRINGS "EOFEOF"
+#define STR_COMP_SAME_VALUE 0
 
 static int save_log(char *filename, char *log);
+
+static char *get_char_from_stdin(char *s, int n);
+
+static char *get_char_from_stdin(char *s, int n)
+{
+   if (fgets(s, n, stdin) == NULL) return NULL;
+   char *ln = strchr(s, '\n');
+   if (ln) *ln = '\0';
+   else while (1) {
+      int c = getchar();
+      if (c == '\n' || c == EOF) break;
+   }
+   return s;
+}
 
 static int save_log(char *filename, char *log)
 {
@@ -21,13 +41,27 @@ static int save_log(char *filename, char *log)
 int device_control_main(void)
 {
     char filename[256];
-    int ii;
+    char buf[COMMUNICATE_BUF_MAX_LEN];
+    int counter = 0;
+    // int ii;
     printf("Hello, next world\n");
-    for(ii=0; ii<5; ii++){
-        sprintf(filename, "file_%d.txt", ii);
-        // printf("%s\n", filename);
-        save_log(filename, filename);
+    while(1){
+        // buffer check
+        if(get_char_from_stdin(buf, COMMUNICATE_BUF_MAX_LEN) == NULL){ continue; }
+        if(strcmp(buf, EOF_STRINGS) == STR_COMP_SAME_VALUE){ break; }
+
+        // exec commands
+        sprintf(filename, "file_%d.txt", counter);
+        save_log(filename, buf);
+        counter += 1;
     }
+    // for(ii=0; ii<5; ii++){
+    //     sprintf(filename, "file_%d.txt", ii);
+    //     // printf("%s\n", filename);
+    //     save_log(filename, filename);
+    // }
+
+    printf("exit while loop\n");
     
     return 0;
 }
