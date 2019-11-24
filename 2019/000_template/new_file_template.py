46e74ce9e8e26b9e9d5b0f2563d08052011c7bd3
toru.yoshihara <toru.ver.11@gmail.com>
2019-11-24 09:01:08 +0900

    added template.

---
+++ 2019/000_template/new_file_template.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+"""
+Title
+==============
+
+Description.
+
+"""
+
+# import standard libraries
+import os
+
+# import third-party libraries
+
+# import my libraries
+
+# information
+__author__ = 'Toru Yoshihara'
+__copyright__ = 'Copyright (C) 2019 - Toru Yoshihara'
+__license__ = 'New BSD License - https://opensource.org/licenses/BSD-3-Clause'
+__maintainer__ = 'Toru Yoshihara'
+__email__ = 'toru.ver.11 at-sign gmail.com'
+
+__all__ = []
+
+
+if __name__ == '__main__':
+    os.chdir(os.path.dirname(os.path.abspath(__file__)))
