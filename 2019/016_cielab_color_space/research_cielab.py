fcb53ded4507fc00c3c7358f4d24323c70a4c562
toru.yoshihara <toru.ver.11@gmail.com>
2019-12-30 09:58:37 +0900

    ようやくバグの原因を掴んだ。
    最初にまとめて計算したのが良くない気がする。
    XYZ の 段階で subs を使って代入してから RGB を計算するように
    コードを修正する。その前にコミット。

--- 2019/016_cielab_color_space/research_cielab.py
+++ 2019/016_cielab_color_space/research_cielab.py
@@ -196,6 +196,7 @@ def plot_formula_for_specific_lstar(
             xyz_t[idx] = np.ones_like(x) * xyz_t[idx]
 
     rgb = [np.zeros_like(rgb_exprs[0][idx]) for idx in range(3)]
+    arg_idxs = [np.zeros_like(rgb_exprs[0][idx]) for idx in range(3)]
     for ii in range(len(IJK_LIST)):
         for jj in range(3):
             x_idx = (xyz_t[0] > SIGMA) if IJK_LIST[ii][0] else (xyz_t[0] <= SIGMA)
@@ -203,6 +204,7 @@ def plot_formula_for_specific_lstar(
             z_idx = (xyz_t[2] > SIGMA) if IJK_LIST[ii][2] else (xyz_t[2] <= SIGMA)
             idx = (x_idx & y_idx) & z_idx
             rgb[jj][idx] = rgb_exprs[ii][jj][idx]
+            arg_idxs[jj][idx] = ii * 0.1
 
     graph_name_0 = "./formula_seq/L0_{:03d}_{:04d}.png".format(l_idx, h_idx)
     graph_name_1 = "./formula_seq/L1_{:03d}_{:04d}.png".format(l_idx, h_idx)
@@ -218,7 +220,7 @@ def plot_formula_for_specific_lstar(
         axis_label_size=None,
         legend_size=17,
         xlim=(-50, 250),
-        ylim=(-0.5, 0.5),
+        ylim=(-0.5, 0.8),
         xtick=[25 * x - 50 for x in range(13)],
         ytick=None,
         xtick_size=None, ytick_size=None,
@@ -228,9 +230,9 @@ def plot_formula_for_specific_lstar(
     ax1.plot(x, rgb[0], 'r-', label="R")
     ax1.plot(x, rgb[1], 'g-', label="G")
     ax1.plot(x, rgb[2], 'b-', label="B")
-    ax1.plot(x, xyz_t[0] > SIGMA, 'r-', alpha=0.3, label="are")
-    ax1.plot(x, xyz_t[1] > SIGMA, 'g-', alpha=0.3, label="are")
-    ax1.plot(x, xyz_t[2] > SIGMA, 'b-', alpha=0.3, label="are")
+    # ax1.plot(x, arg_idxs[0], 'r-', alpha=0.3, label="arg_idx")
+    # ax1.plot(x, arg_idxs[1], 'g-', alpha=0.3, label="arg_idx")
+    ax1.plot(x, arg_idxs[2], 'b-', alpha=0.3, label="arg_idx")
     plt.legend(loc='upper left')
     plt.savefig(graph_name_0, bbox_inches='tight', pad_inches=0.1)
     # plt.show()
@@ -273,32 +275,45 @@ def solve_chroma(l_val, l_idx, h_val, h_idx, rgb_exprs, l, c, h):
     xyz_t = [get_tx(l, c, h), get_ty(l), get_tz(l, c, h)]
     xyz_t = [xyz_t[idx].subs({l: l_val, h: h_val}) for idx in range(3)]
     temp_solution = []
+    print(xyz_t)
     for ii in range(len(IJK_LIST)):
-        for jj in range(3):
+        for jj in range(3):  # R, G, B のループ
             # l_val, h_val 代入
             c_expr = rgb_exprs[ii][jj].subs({l: l_val, h: h_val})
-            solution_zero = solve(c_expr + 0)
-            solution_one = solve(c_expr - 1)
+            solution = []
+            print(c_expr)
+            solution.extend(solve(c_expr))
+            solution.extend(solve(c_expr - 1))
+            # solution_zero = solve(c_expr)
+            # solution_one = solve(c_expr - 1)
+            print("ii:{}, jj:{}, solution:{}".format(ii, jj, solution))
 
-            for solve_val in solution_zero:
+            for solve_val in solution:
                 t = [xyz_t[kk].subs({c: solve_val}) for kk in range(3)]
+                # print(t)
                 xt_bool = (t[0] > SIGMA) if IJK_LIST[ii][0] else (t[0] <= SIGMA)
                 yt_bool = (t[1] > SIGMA) if IJK_LIST[ii][1] else (t[1] <= SIGMA)
                 zt_bool = (t[2] > SIGMA) if IJK_LIST[ii][2] else (t[2] <= SIGMA)
+                print(xt_bool, yt_bool, zt_bool)
                 xyz_t_bool = (xt_bool and yt_bool) and zt_bool
                 if xyz_t_bool:
                     temp_solution.append(solve_val)
 
-            for solve_val in solution_one:
-                t = [xyz_t[kk].subs({c: solve_val}) for kk in range(3)]
-                xt_bool = (t[0] > SIGMA) if IJK_LIST[ii][0] else (t[0] <= SIGMA)
-                yt_bool = (t[1] > SIGMA) if IJK_LIST[ii][1] else (t[1] <= SIGMA)
-                zt_bool = (t[2] > SIGMA) if IJK_LIST[ii][2] else (t[2] <= SIGMA)
-                xyz_t_bool = (xt_bool and yt_bool) and zt_bool
-                if xyz_t_bool:
-                    temp_solution.append(solve_val)
+            # for solve_val in solution_one:
+            #     t = [xyz_t[kk].subs({c: solve_val}) for kk in range(3)]
+            #     # print(t)
+            #     xt_bool = (t[0] > SIGMA) if IJK_LIST[ii][0] else (t[0] <= SIGMA)
+            #     yt_bool = (t[1] > SIGMA) if IJK_LIST[ii][1] else (t[1] <= SIGMA)
+            #     zt_bool = (t[2] > SIGMA) if IJK_LIST[ii][2] else (t[2] <= SIGMA)
+            #     print(xt_bool, yt_bool, zt_bool)
+            #     xyz_t_bool = (xt_bool and yt_bool) and zt_bool
+            #     print(xyz_t_bool)
+            #     if xyz_t_bool:
+            #         temp_solution.append(solve_val)
 
+    print(temp_solution)
     chroma_list = np.array(temp_solution)
+    print(chroma_list)
     chroma = np.min(chroma_list[chroma_list >= 0.0])
     print(chroma)
 
@@ -315,11 +330,19 @@ def make_chroma_array():
     l_vals = np.linspace(0, 100, l_sample_num)
     h_vals = np.linspace(0, 2*np.pi, h_sample_num)
     for l_idx, l_val in enumerate(l_vals):
+        if l_idx == 0:
+            continue
         args = []
         for h_idx, h_val in enumerate(h_vals):
+            print("===========================")
+            print("===========================")
+            print(h_idx)
+            print("===========================")
+            print("===========================")
             args.append([l_val, l_idx, h_val, h_idx, rgb_exprs, l, c, h])
             solve_chroma(l_val, l_idx, h_val, h_idx, rgb_exprs, l, c, h)
-
+            if h_idx == 1 and l_idx == 1:
+                return None
         # with Pool(cpu_count()) as pool:
         #     pool.map(thread_wrapper_visualization_formula, args)
 
@@ -332,6 +355,10 @@ def experimental_functions():
 if __name__ == '__main__':
     os.chdir(os.path.dirname(os.path.abspath(__file__)))
     start = time.time()
-    experimental_functions()
+    # experimental_functions()
     end = time.time()
     print("total_time={}[s]".format(end-start))
+    c = symbols('c', real=True)
+    expr = -0.0006587933118851*c + 0.0167674289909477*(1.22464679914735e-19*c + 0.425287356321839)**3 + 0.0345712150974614
+    print(expr)
+    print(solve(expr))
