a7c92685190e914747fee26d8038d8f835a90ed6
toru.yoshihara <toru.ver.11@gmail.com>
2020-02-27 21:13:18 +0900

    #61 support bg image for make_moving_ball.

--- 2020/003_make_moving_ball_class/plot_moving_ball.py
+++ 2020/003_make_moving_ball_class/plot_moving_ball.py
@@ -14,6 +14,8 @@ import os
 import numpy as np
 from numpy.random import rand, seed
 import cv2
+import test_pattern_generator2 as tpg
+import transfer_functions as tf
 
 # import my libraries
 from reflective_moving_object import ReflectiveMovingObject
@@ -55,6 +57,43 @@ def main_func():
         cv2.imwrite(fname, img[:, :, ::-1])
 
 
+def plot_with_bg_image():
+    radius = 20
+    bg_level = 0
+    velocity_rate = 20
+    seed(0)
+    velocity_list = np.array([rand(2), rand(2), rand(2), rand(2)])
+    velocity_list = np.int16(np.round(velocity_list * velocity_rate))
+    color_list = [(192, 192, 192), (192, 0, 0),
+                  (0, 192, 0), (0, 0, 192)]
+    bg_image = cv2.imread(
+        "./bg_image.tiff", cv2.IMREAD_ANYCOLOR | cv2.IMREAD_ANYDEPTH)
+    bg_image = bg_image[:, :, ::-1] / 0xFFFF
+
+    rmo_list = [ReflectiveMovingObject(
+        velocity_init=velocity_list[idx], radius=radius)
+        for idx in range(len(velocity_list))]
+    trial_num = 720
+    img_base = np.zeros((1080, 1920, 3), dtype=np.uint8)
+    for idx in range(trial_num):
+        fname = "./img_with_bg_image/ball_size_{:03d}_lv_{:04d}.png".format(
+            radius, idx)
+        pos_list = [
+            rmo_list[idx].get_pos() for idx in range(len(velocity_list))]
+        img = img_base.copy()
+        for idx, pos in enumerate(pos_list):
+            img = cv2.circle(
+                img, pos_list[idx], radius,
+                color=color_list[idx], thickness=-1)
+            rmo_list[idx].calc_next_pos()
+        alpha = np.max(img, axis=-1)
+        img = np.dstack((img, alpha)) / 0xFF
+        bg_temp = bg_image.copy()
+        img = tpg.merge_with_alpha(bg_temp, img, tf.ST2084)
+        cv2.imwrite(fname, np.uint16(np.round(img[:, :, ::-1] * 0xFFFF)))
+
+
 if __name__ == '__main__':
     os.chdir(os.path.dirname(os.path.abspath(__file__)))
-    main_func()
+    # main_func()
+    plot_with_bg_image()
