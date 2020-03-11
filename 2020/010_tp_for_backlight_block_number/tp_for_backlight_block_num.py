2335939f5e1d494b481073ace7f612d0e6754f17
toruver4 <toru.ver.11@gmail.com>
2020-03-11 19:32:37 +0900

    #73 3種類の確認用パターンを作成

--- 2020/010_tp_for_backlight_block_number/tp_for_backlight_block_num.py
+++ 2020/010_tp_for_backlight_block_number/tp_for_backlight_block_num.py
@@ -51,8 +51,51 @@ def draw_rectangle(img, square_obj, size, st_pos_h, st_pos_v):
     return img
 
 
-def save_image(img, width, height, size, frame_rate, frame_idx):
+def draw_rectangle_center(img, square_obj, size, st_pos_h, st_pos_v):
+    """
+    Parameters
+    ----------
+    img : array_like (np.uint8)
+        background_image
+    size : int
+        the size of the bright square object.
+    st_pos_h : int
+        start position of the square object that moves horizontally.
+    st_pos_v : int
+        start position of the square object that moves vertically.
+
+    Returns
+    -------
+    img : array_like (np.int8)
+        image with two rectangle.
+    """
+    width = img.shape[1]
+    height = img.shape[0]
+    st_pos_v2 = (height // 2) - (size // 2)
+    st_pos_h2 = (width // 2) - (size // 2)
+    tpg.merge(img, square_obj, (st_pos_h, st_pos_v2))  # H方向に動く正方形
+    tpg.merge(img, square_obj, (st_pos_h2, st_pos_v))  # V方向に動く正方形
+
+    return img
+
+
+def save_constant_velocity_image(
+        img, width, height, size, frame_rate, frame_idx):
     fname = f"./sequence/tp_{width}x{height}_{frame_rate}p_{frame_idx:04d}.png"
+    cv2.imwrite(fname, img)
+
+
+def save_variable_velocity_image(
+        img, width, height, size, frame_rate, frame_idx, sec):
+    fname = f"./sequence/tp_variagle_ul_{width}x{height}_{frame_rate}p_"\
+        + f"{sec}s_{frame_idx:04d}.png"
+    cv2.imwrite(fname, img)
+
+
+def save_variable_velocity_center_image(
+        img, width, height, size, frame_rate, frame_idx, sec):
+    fname = f"./sequence/tp_variagle_cc_{width}x{height}_{frame_rate}p_"\
+        + f"{sec}s_{frame_idx:04d}.png"
     cv2.imwrite(fname, img)
 
 
@@ -92,7 +135,8 @@ def constant_velocity_linear_motion(
     temp_img = draw_rectangle(
         img=base_img.copy(), square_obj=square_obj,
         size=size, st_pos_h=0, st_pos_v=0)
-    save_image(temp_img, width, height, size, frame_rate, frame_idx=0)
+    save_constant_velocity_image(
+        temp_img, width, height, size, frame_rate, frame_idx=0)
 
     for idx in range(1, frame_num):
         st_pos_h = np.sum(h_distance_list[:idx])
@@ -100,18 +144,154 @@ def constant_velocity_linear_motion(
         temp_img = draw_rectangle(
             img=base_img.copy(), square_obj=square_obj, size=size,
             st_pos_h=st_pos_h, st_pos_v=st_pos_v)
-        save_image(
+        save_constant_velocity_image(
             img=temp_img, width=width, height=height, size=size,
             frame_rate=frame_rate, frame_idx=idx)
 
 
+def get_accelerated_x(sample_num=64):
+    rad = np.linspace(-0.5 * np.pi, 0.5 * np.pi, sample_num)
+    rad = np.sin(rad) * 0.5 * np.pi
+    x = (np.sin(rad) + 1) / 2
+
+    return x
+
+
+def variable_velocity_linear_motion(
+        width=1920, height=1080, size=64, frame_rate=60, second=10):
+    """
+    Parameters
+    ----------
+    width : int
+        the width of the test pattern video
+    height : int
+        the height of the test pattern video
+    size : int
+        the size of the bright square object.
+    frame_rate : int
+        framerate of the test pattern video
+    second : int
+        time length of the test pattern video
+
+    Returns
+    -------
+    None
+        png sequence files are saved to "sequence" directory.
+    """
+    frame_num = int(frame_rate * second)
+    h_distance_total = width - size
+    v_distance_total = height - size
+
+    # "-1" の計算は始点に関しては計算の必要が無いため
+    x = get_accelerated_x(frame_num)
+
+    h_distance_list = x * h_distance_total
+    v_distance_list = x * v_distance_total
+
+    base_img = np.zeros((height, width, 3), dtype=np.uint8)
+    square_obj = np.ones((size, size, 3), dtype=np.uint8) * 192
+
+    for idx in range(frame_num):
+        st_pos_h = int(h_distance_list[idx] + 0.5)
+        st_pos_v = int(v_distance_list[idx] + 0.5)
+        temp_img = draw_rectangle(
+            img=base_img.copy(), square_obj=square_obj, size=size,
+            st_pos_h=st_pos_h, st_pos_v=st_pos_v)
+        save_variable_velocity_image(
+            img=temp_img, width=width, height=height, size=size,
+            frame_rate=frame_rate, frame_idx=idx, sec=second)
+
+    for idx in range(frame_num):
+        st_pos_h = int(h_distance_list[idx] + 0.5)
+        st_pos_v = int(v_distance_list[idx] + 0.5)
+        temp_img = draw_rectangle(
+            img=base_img.copy(), square_obj=square_obj, size=size,
+            st_pos_h=st_pos_h, st_pos_v=st_pos_v)
+        save_variable_velocity_image(
+            img=temp_img, width=width, height=height, size=size,
+            frame_rate=frame_rate, frame_idx=frame_num * 2 - idx - 1,
+            sec=second)
+
+
+def variable_velocity_linear_motion_center(
+        width=1920, height=1080, size=64, frame_rate=60, second=10):
+    """
+    Parameters
+    ----------
+    width : int
+        the width of the test pattern video
+    height : int
+        the height of the test pattern video
+    size : int
+        the size of the bright square object.
+    frame_rate : int
+        framerate of the test pattern video
+    second : int
+        time length of the test pattern video
+
+    Returns
+    -------
+    None
+        png sequence files are saved to "sequence" directory.
+    """
+    frame_num = int(frame_rate * second)
+    h_distance_total = width - size
+    v_distance_total = height - size
+
+    # "-1" の計算は始点に関しては計算の必要が無いため
+    x = get_accelerated_x(frame_num)
+
+    h_distance_list = x * h_distance_total
+    v_distance_list = x * v_distance_total
+
+    base_img = np.zeros((height, width, 3), dtype=np.uint8)
+    square_obj = np.ones((size, size, 3), dtype=np.uint8) * 192
+
+    for idx in range(frame_num):
+        st_pos_h = int(h_distance_list[idx] + 0.5)
+        st_pos_v = int(v_distance_list[idx] + 0.5)
+        temp_img = draw_rectangle_center(
+            img=base_img.copy(), square_obj=square_obj, size=size,
+            st_pos_h=st_pos_h, st_pos_v=st_pos_v)
+        save_variable_velocity_center_image(
+            img=temp_img, width=width, height=height, size=size,
+            frame_rate=frame_rate, frame_idx=idx, sec=second)
+
+    for idx in range(frame_num):
+        st_pos_h = int(h_distance_list[idx] + 0.5)
+        st_pos_v = int(v_distance_list[idx] + 0.5)
+        temp_img = draw_rectangle_center(
+            img=base_img.copy(), square_obj=square_obj, size=size,
+            st_pos_h=st_pos_h, st_pos_v=st_pos_v)
+        save_variable_velocity_center_image(
+            img=temp_img, width=width, height=height, size=size,
+            frame_rate=frame_rate, frame_idx=frame_num * 2 - idx - 1,
+            sec=second)
+
+
 def main_func():
+    # width = 1920
+    # height = 1080
+    # fps = 60
+    # sec = 20
+    # size = width // 16
+    # constant_velocity_linear_motion(
+    #     width=width, height=height, size=size, frame_rate=fps, second=sec)
+
     width = 1920
     height = 1080
     fps = 60
-    sec = 10
-    size = width // 32
-    constant_velocity_linear_motion(
+    sec = 1.5
+    size = width // 16
+    variable_velocity_linear_motion(
+        width=width, height=height, size=size, frame_rate=fps, second=sec)
+
+    width = 1920
+    height = 1080
+    fps = 60
+    sec = 1.5
+    size = width // 16
+    variable_velocity_linear_motion_center(
         width=width, height=height, size=size, frame_rate=fps, second=sec)
 
 
