9db2ed1be1b47a17cbb2ee31aede2836317431eb
toru.yoshihara <toru.ver.11@gmail.com>
2020-08-05 22:33:22 +0900

    #112 add diagonal line

--- 2020/023_plot_color_volume_with_wireframe/plot_color_volume.py
+++ 2020/023_plot_color_volume_with_wireframe/plot_color_volume.py
@@ -21,6 +21,7 @@ from mpl_toolkits.mplot3d import Axes3D
 import color_space as cs
 from common import MeasureExecTime
 import plot_utility as pu
+import test_pattern_generator2 as tpg
 
 # information
 __author__ = 'Toru Yoshihara'
@@ -196,6 +197,8 @@ def calc_xyY_boundary_data(
         mtime.lap()
     mtime.end()
 
+    out_buf[0] = out_buf[1].copy()
+
     np.save(fname, out_buf)
     return out_buf
 
@@ -239,7 +242,7 @@ def plot_simple_xy_plane(xyY_data):
     angle = calc_angle_from_ndarray(data)
     # 前処理以上データは 180°にしてしまう
     angle[angle < 1] = 180
-    # 小さい値15点を抽出
+    # 小さい値を reduced_sample 個だけ抽出
     rd_idx = np.argsort(angle)[:reduced_sample]
 
     ax1.plot(
@@ -271,6 +274,10 @@ def plot_xyY_color_volume(
 
     fig = plt.figure(figsize=(9, 9))
     ax = Axes3D(fig)
+    plt.gca().patch.set_facecolor('white')
+    ax.w_xaxis.set_pane_color((0.6, 0.6, 0.6, 1.0))
+    ax.w_yaxis.set_pane_color((0.6, 0.6, 0.6, 1.0))
+    ax.w_zaxis.set_pane_color((0.6, 0.6, 0.6, 1.0))
     ax.set_xlabel("x")
     ax.set_ylabel("y")
     ax.set_zlabel("Y")
@@ -279,26 +286,66 @@ def plot_xyY_color_volume(
     #     fontsize=18)
     ax.set_xlim(0.0, 0.8)
     ax.set_ylim(0.0, 0.9)
-    # ax.set_zlim(0.0, 1.1)
+    ax.set_zlim(0.0, 1.1)
     ax.view_init(elev=20, azim=-120)
     ax.scatter(x, y, z, marker='o', c=rgb, zorder=1)
 
-    for idx in range(1, len(xyY_org), 24):
-        x = xyY_org[idx, ..., 0].flatten()
-        y = xyY_org[idx, ..., 1].flatten()
-        z = (np.ones_like(x) * idx / (len(xyY_org) - 1)).flatten()
+    # for idx in range(1, len(xyY_org), 24):
+    #     x = xyY_org[idx, ..., 0].flatten()
+    #     y = xyY_org[idx, ..., 1].flatten()
+    #     z = (np.ones_like(x) * idx / (len(xyY_org) - 1)).flatten()
 
-        large_xyz = xyY_to_XYZ(np.dstack((x, y, z)))
-        rgb = XYZ_to_RGB(
-            large_xyz, cs.D65, cs.D65,
-            RGB_COLOURSPACES[color_space_name].XYZ_to_RGB_matrix)
-        rgb = np.clip(rgb, 0.0, 1.0) ** (1/2.4)
-        rgb = rgb.reshape((len(x), 3))
-        ax.scatter(x, y, z, marker='o', c=rgb, s=1)
+    #     ax.scatter(x, y, z, marker='o', c=rgb, s=1)
     # ax.plot(x, y, z)
-    # plt.savefig(graph_name, bbox_inches='tight', pad_inches=0.1)
-    plt.show()
+    st_offset_list = np.linspace(0, 1, 40, endpoint=False)
+    for st_offset in st_offset_list:
+        x, y, z = extract_screw_data(
+            xyY_org, y_step=1, rad_st_offset=st_offset, rad_rate=4.0)
+        rgb = get_rgb_from_x_y_z(x, y, z)
+        ax.scatter(x, y, z, s=1, c=rgb)
+        x, y, z = extract_screw_data(
+            xyY_org, y_step=1, rad_st_offset=st_offset, rad_rate=-4.0)
+        rgb = get_rgb_from_x_y_z(x, y, z)
+        ax.scatter(x, y, z, s=1, c=rgb)
+    angle_list = np.linspace(-120, -120+360, 360, endpoint=False)
+    for idx, angle in enumerate(angle_list):
+        ax.view_init(elev=20, azim=angle)
+        fname = f"./img_seq/angle_{idx:04d}.png"
+        print(fname)
+        plt.savefig(
+            fname, bbox_inches='tight', pad_inches=0.1)
+    # plt.show()
     plt.close(fig)
+
+
+def get_rgb_from_x_y_z(x, y, z, color_space_name=cs.BT2020):
+    large_xyz = xyY_to_XYZ(np.dstack((x, y, z)))
+    rgb = XYZ_to_RGB(
+        large_xyz, cs.D65, cs.D65,
+        RGB_COLOURSPACES[color_space_name].XYZ_to_RGB_matrix)
+    rgb = np.clip(rgb, 0.0, 1.0) ** (1/2.4)
+    rgb = rgb.reshape((len(x), 3))
+
+    return rgb
+
+
+def extract_screw_data(xyY_data, y_step=1, rad_st_offset=0.0, rad_rate=1.5):
+    large_y_num = len(xyY_data[::y_step])
+    xy_sample = len(xyY_data[0])
+    xy_step = tpg.equal_devision(int(large_y_num * rad_rate), xy_sample)
+    x_buf = []
+    y_buf = []
+    z_buf = []
+    xy_idx = int(rad_st_offset * xy_sample)
+    xy_cnt = 0
+    for idx in range(0, len(xyY_data), y_step):
+        x_buf.append(xyY_data[idx, xy_idx % xy_sample, 0])
+        y_buf.append(xyY_data[idx, xy_idx % xy_sample, 1])
+        z_buf.append(idx / (len(xyY_data) - 1))
+        xy_idx += xy_step[xy_cnt]
+        xy_cnt += 1
+
+    return np.array(x_buf), np.array(y_buf), np.array(z_buf)
 
 
 def experimental_func():
@@ -308,10 +355,11 @@ def experimental_func():
     # print(data.shape)
     # calc_angle_from_ndarray(data=data)
     xyY_data = calc_xyY_boundary_data(
-        color_space_name=cs.BT2020, y_num=512, h_num=1024)
+        color_space_name=cs.BT2020, y_num=513, h_num=1024)
     # plot_simple_xy_plane(xyY_data)
-    reduced_xyY_data = reduce_xyY_sample(xyY_data=xyY_data, reduced_sample=8)
+    reduced_xyY_data = reduce_xyY_sample(xyY_data=xyY_data, reduced_sample=6)
     plot_xyY_color_volume(xyY_data=reduced_xyY_data, xyY_org=xyY_data)
+    # extract_screw_data(xyY_data, y_step=1, rad_st_offset=0.0, rad_rate=4.2)
 
 
 if __name__ == '__main__':
