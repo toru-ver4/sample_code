d5187c78b00657fc3d8f55d2b7c30fd25236b9e0
toru.yoshihara <toru.ver.11@gmail.com>
2020-09-10 21:18:14 +0900

    #118 under implementation

--- 2020/026_create_icc_profiles/icc_profile_ana.py
+++ 2020/026_create_icc_profiles/icc_profile_ana.py
@@ -11,7 +11,8 @@ import xml.etree.ElementTree as ET
 
 # import third-party libraries
 import numpy as np
-from colour.models import ACES_CG_COLOURSPACE, ACES_2065_1_COLOURSPACE, RGB_COLOURSPACES
+from colour.models import ACES_CG_COLOURSPACE,\
+    ACES_2065_1_COLOURSPACE, RGB_COLOURSPACES
 from colour import RGB_to_RGB
 
 # import my libraries
@@ -112,10 +113,177 @@ def get_cprt_element(root):
         key_tag="TagSignature", key_text="cprt", target_tag="LocalizedText")
 
 
+def get_parametric_curve_element(root):
+    parametric_curve_element = None
+    parent_tag = 'parametricCurveType'
+    key_tag = 'ParametricCurve'
+    for parent_element in root.iter(parent_tag):
+        if parent_element.find(key_tag) is not None:
+            parametric_curve_element = parent_element.find(key_tag)
+
+    return parametric_curve_element
+
+
+def get_chad_matrix_from_chad_mtx_element(chad_mtx_element):
+    """
+    Parameters
+    ----------
+    chad_mtx_element : xml.etree.ElementTree.Element
+        An instance of the Element class. It indicate the chad tag.
+
+    Returns
+    -------
+    ndarray
+        chromatic adaptation matrix.
+
+    Examples
+    --------
+    >>> tree = ET.parse("./aces_ap0_gm30.xml")
+    >>> root = tree.getroot()
+    >>> chad_mtx_element = get_chad_mtx_element(root)
+    >>> get_chad_matrix_from_chad_mtx_element(chad_mtx_element)
+    [[ 1.04788208  0.02958679 -0.00924683]
+     [ 0.0229187   0.99047852  0.01507568]
+     [-0.05021667 -0.01707458  0.75167847]]
+    """
+    text_data = chad_mtx_element.text
+    text_mtx_list = text_data.strip().replace("\n", "").split()
+    mtx = np.array([float(x) for x in text_mtx_list]).reshape((3, 3))
+
+    return mtx
+
+
+def set_chad_matrix_to_chad_mtx_element(mtx, chad_mtx_element):
+    """
+    Parameters
+    ----------
+    mtx : ndarray
+        chromatic adaptation matrix
+    chad_mtx_element : xml.etree.ElementTree.Element
+        An instance of the Element class. It indicate the chad tag.
+
+    Returns
+    -------
+    None
+
+    Examples
+    --------
+    >>> tree = ET.parse("./aces_ap0_gm30.xml")
+    >>> root = tree.getroot()
+    >>> chad_mtx_element = get_chad_mtx_element(root)
+    >>> print(chad_mtx_element.text)
+
+                    1.04788208 0.02291870 -0.05021667
+                    0.02958679 0.99047852 -0.01707458
+                    -0.00924683 0.01507568 0.75167847
+
+    >>> mtx = np.array([
+    ...     [0.1, 0.2, 0.3],
+    ...     [0.4, 0.5, 0.6],
+    ...     [0.7, 0.8, 0.9]])
+    >>> set_chad_matrix_to_chad_mtx_element(mtx, chad_mtx_element)
+    >>> print(chad_mtx_element.text)
+
+                    0.10000000 0.20000000 0.30000000
+                    0.40000000 0.50000000 0.60000000
+                    0.70000000 0.80000000 0.90000000
+
+    """
+    head_space = " " * 12
+    foot_space = " " * 6
+    buf = "\n"
+    buf += f"{head_space}{mtx[0][0]:.8f} {mtx[0][1]:.8f} {mtx[0][2]:.8f}\n"
+    buf += f"{head_space}{mtx[0][0]:.8f} {mtx[0][1]:.8f} {mtx[0][2]:.8f}\n"
+    buf += f"{head_space}{mtx[0][0]:.8f} {mtx[0][1]:.8f} {mtx[0][2]:.8f}\n"
+    buf += foot_space
+
+    chad_mtx_element.text = buf
+
+
+def get_parametric_curve_params_from_element(parameteric_curve_element):
+    """
+    Parameters
+    ----------
+    parameteric_curve_element : xml.etree.ElementTree.Element
+        An instance of the Element class. It indicate the ParametricCurve tag.
+
+    Returns
+    -------
+    function_type_str : str
+        A string of function type.
+    params : ndarray
+        A one-dimensional parameter array.
+
+    Examples
+    --------
+    >>> tree = ET.parse("./p3-2.xml")
+    >>> root = tree.getroot()
+    >>> parameteric_curve_element = get_parametric_curve_element(root)
+    >>> function_type, params = get_parametric_curve_params_from_element(
+    ...     parameteric_curve_element)
+    >>> print(function_type)
+    3
+    >>> print(params)
+    [ 2.39999390 0.94786072 0.05213928 0.07739258 0.04045105 ]
+    """
+    function_type_str = parameteric_curve_element.attrib['FunctionType']
+    text_data = parameteric_curve_element.text
+    param_str_list = text_data.strip().replace("\n", "").split()
+    param_ndarray = np.array([float(x) for x in param_str_list])
+
+    return function_type_str, param_ndarray
+
+
+def get_parametric_curve_params_to_element(
+        function_type_str, params, parameteric_curve_element):
+    """
+    Parameters
+    ----------
+    function_type_str : str
+        A function type.
+    params : ndarray
+        An array of the function parameters.
+    parameteric_curve_element : xml.etree.ElementTree.Element
+        An instance of the Element class. It indicate the ParametricCurve tag.
+
+    Returns
+    -------
+    None
+
+    Examples
+    --------
+    >>> tree = ET.parse("./p3-2.xml")
+    >>> root = tree.getroot()
+    >>> parameteric_curve_element = get_parametric_curve_element(root)
+    >>> function_type = "2"
+    >>> params = np.array([2.4, 0.1, 0.2, 0.3])
+    >>> get_parametric_curve_params_to_element(
+    ...     function_type, params, parameteric_curve_element)
+    >>> print(parameteric_curve_element.attrib)
+    {'FunctionType': '2'}
+    >>> print(parameteric_curve_element.text)
+
+            2.39999390 0.10000000 0.20000000 0.30000000
+
+    """
+    parameteric_curve_element.attrib['FunctionType'] = function_type_str
+
+    head_space = '\n' + " " * 8
+    foot_space = " " * 6
+    buf = head_space
+    param_str_array = [f"{x:.8f}" for x in params]
+    buf += " ".join(param_str_array) + '\n' + foot_space
+    parameteric_curve_element.text = buf
+
+
 def xml_parse_test():
-    tree = ET.parse("./aces_ap0_gm30.xml")
+    tree = ET.parse("./p3-2.xml")
     root = tree.getroot()
     chad_mtx_element = get_chad_mtx_element(root)
+    print(chad_mtx_element.text)
+    get_chad_matrix_from_chad_mtx_element(chad_mtx_element)
+    set_chad_matrix_to_chad_mtx_element(
+        mtx=np.ones((3, 3)), chad_mtx_element=chad_mtx_element)
     print(chad_mtx_element.text)
     desc_element = get_desc_element(root)
     print(desc_element.text)
@@ -125,6 +293,21 @@ def xml_parse_test():
     print(profile_ver_element.text)
     color_space_element = get_icc_header_element(root, tag="DataColourSpace")
     print(color_space_element.text)
+    parameteric_curve_element = get_parametric_curve_element(root)
+    print(parameteric_curve_element.text)
+    function_type, params = get_parametric_curve_params_from_element(
+        parameteric_curve_element)
+    print(function_type)
+    print(params)
+
+    function_type = "2"
+    params = np.array([2.4, 0.1, 0.2, 0.3])
+    get_parametric_curve_params_to_element(
+        function_type, params, parameteric_curve_element)
+    print(parameteric_curve_element.attrib)
+    print(parameteric_curve_element.text)
+
+    tree.write("test_out.xml")
 
 
 if __name__ == '__main__':
