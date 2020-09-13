3260f47f59d17c3a9c6e322961d3d34fc3c6f8e8
toru.yoshihara <toru.ver.11@gmail.com>
2020-09-13 22:46:42 +0900

    #119 サンプルコード完成！

--- 2020/027_excel_control_by_python/excel_control_sample.py
+++ 2020/027_excel_control_by_python/excel_control_sample.py
@@ -20,6 +20,46 @@ def rgb_8bit_to_24bit(r=192, g=128, b=255):
     return r * 0x00000001 + g * 0x00000100 + b * 0x00010000
 
 
+def change_row_height(ws, height, st_pos_row, ed_pos_row):
+    """
+    Row の 高さを調整
+
+    Parameters
+    ----------
+    ws : win32com.client.CDispatch
+        excel worksheet
+    height : int
+        height of the raw.
+    st_pos_row : int
+        start position of row.
+    ed_pos_row : int
+        end position of row. It is not included.
+    """
+    range = f"{st_pos_row}:{ed_pos_row - 1}"
+    ws.Rows(range).RowHeight = height
+
+
+def change_col_width(ws, width, st_pos_col, ed_pos_col):
+    """
+    Column の 幅を調整
+
+    Parameters
+    ----------
+    ws : win32com.client.CDispatch
+        excel worksheet
+    height : int
+        height of the raw.
+    st_pos_col : int
+        start position of row.
+    ed_pos_col : int
+        end position of colmun. It is not included.
+    """
+    # range = f"{st_pos_col}:{ed_pos_col - 1}"
+    st_cell = ws.Cells(1, st_pos_col)
+    ed_cell = ws.Cells(1, ed_pos_col - 1)
+    ws.Range(st_cell, ed_cell).ColumnWidth = width
+
+
 def create_color_table_1d(
         ws, cell_num=24, st_pos_row=2, st_pos_col=3):
     """
@@ -86,44 +126,34 @@ def create_color_table_2d(
             cell.Interior.Color = color_value
 
 
-def change_row_height(ws, height, st_pos_row, ed_pos_row):
+def sequential_process(ws, st_pos_row=2, st_pos_col=3):
     """
-    Row の 高さを調整
-
-    Parameters
-    ----------
-    ws : win32com.client.CDispatch
-        excel worksheet
-    height : int
-        height of the raw.
-    st_pos_row : int
-        start position of row.
-    ed_pos_row : int
-        end position of row. It is not included.
+    適当にセルを範囲選択して wirte/read してみる。
     """
-    range = f"{st_pos_row}:{ed_pos_row - 1}"
-    ws.Rows(range).RowHeight = height
+    sample_num = 56
 
+    # とりあえず Gamma=2.4 のデータを書いてみる
+    # まずは Index の書き込み
+    pos_col = st_pos_col
+    for idx in range(sample_num):
+        pos_row = st_pos_row + idx
+        cell = ws.Cells(pos_row, pos_col)
+        cell.Value = idx
 
-def change_col_width(ws, width, st_pos_col, ed_pos_col):
-    """
-    Column の 幅を調整
+        linear_val = (idx / sample_num) ** 2.4
+        cell = ws.Cells(pos_row, pos_col + 1)
+        cell.Value = linear_val
 
-    Parameters
-    ----------
-    ws : win32com.client.CDispatch
-        excel worksheet
-    height : int
-        height of the raw.
-    st_pos_col : int
-        start position of row.
-    ed_pos_col : int
-        end position of colmun. It is not included.
-    """
-    # range = f"{st_pos_col}:{ed_pos_col - 1}"
-    st_cell = ws.Cells(1, st_pos_col)
-    ed_cell = ws.Cells(1, ed_pos_col - 1)
-    ws.Range(st_cell, ed_cell).ColumnWidth = width
+    # 高速版のIndex 書き込み
+    st_cel = ws.Cells(st_pos_row, st_pos_col + 2)
+    ed_cel = ws.Cells(st_pos_row + sample_num - 1, st_pos_col + 2)
+    ws.Range(st_cel, ed_cel).Value = [[x] for x in range(sample_num)]
+
+    # 高速版のEOTF値 書き込み
+    st_cel = ws.Cells(st_pos_row, st_pos_col + 3)
+    ed_cel = ws.Cells(st_pos_row + sample_num - 1, st_pos_col + 3)
+    ws.Range(st_cel, ed_cel).Value = [
+        [(x / sample_num) ** 2.4] for x in range(sample_num)]
 
 
 def main_func():
@@ -148,14 +178,20 @@ def main_func():
     create_color_table_2d(
         ws, cell_row_num=9, cell_col_num=16, st_pos_row=2, st_pos_col=3)
 
+    # シート3 でセルの連続選択＆書き込みの実験
+    wb.Sheets.Add(Before=ws)
+    ws = wb.Worksheets(1)
+    ws.Name = "Gamma2.4"
+    sequential_process(ws=ws, st_pos_row=2, st_pos_col=3)
+
     # 保存
     ecu.save_excel_file(filename="./sample.xlsx", wb=wb, excel_app=excel_app)
 
     # # ファイルを閉じる
-    # ecu.close_excel_file(wb)
+    ecu.close_excel_file(wb)
 
     # # エクセルアプリの終了
-    # ecu.quit_excel_app(excel_app=excel_app)
+    ecu.quit_excel_app(excel_app=excel_app)
 
 
 if __name__ == '__main__':
