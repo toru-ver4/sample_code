469a5ba5d961ad1ced9c6d58054cc59c3e062b73
toru.yoshihara <toru.ver.11@gmail.com>
2021-02-15 23:50:52 +0900

    #134 create test patch. not validated.

---
+++ 2021/02_8bit_10bit_pattern_retry/create_8bit_10bit_pattern.py
@@ -0,0 +1,99 @@
+# -*- coding: utf-8 -*-
+"""
+fix alpha blending of font_control
+===================================
+"""
+
+# import standard libraries
+import os
+
+# import third-party libraries
+import numpy as np
+
+# import my libraries
+import test_pattern_generator2 as tpg
+
+# information
+__author__ = 'Toru Yoshihara'
+__copyright__ = 'Copyright (C) 2021 - Toru Yoshihara'
+__license__ = 'New BSD License - https://opensource.org/licenses/BSD-3-Clause'
+__maintainer__ = 'Toru Yoshihara'
+__email__ = 'toru.ver.11 at-sign gmail.com'
+
+__all__ = []
+
+
+BASE_RGB_8BIT = np.array([123, 98, 74])
+GREEN_DIFF = 10
+
+
+def create_test_patch(patch_len=512):
+    width = patch_len
+    height = patch_len
+    base_gg = BASE_RGB_8BIT[1]
+    rr = BASE_RGB_8BIT[0]
+    bb = BASE_RGB_8BIT[2]
+
+    gg_min = base_gg - GREEN_DIFF
+    gg_max = base_gg + GREEN_DIFF
+
+    gg_grad = np.linspace(gg_min, gg_max, patch_len)
+    rr_static = np.ones_like(gg_grad) * rr
+    bb_static = np.ones_like(gg_grad) * bb
+    line = np.dstack((rr_static, gg_grad, bb_static))
+
+    img_base_8bit_float = line * np.ones((height, 1, 3))
+
+    img_out_float_8bit = img_base_8bit_float / 255
+    img_out_8bit = np.round(img_out_float_8bit * 255) / 255
+    img_out_10bit = np.round(img_out_float_8bit * 1023) / 1023
+
+    tpg.img_wirte_float_as_16bit_int("test_8bit.png", img_out_8bit)
+    tpg.img_wirte_float_as_16bit_int("test_10bit.png", img_out_10bit)
+
+
+def create_8bit_10bit_patch(
+        width=512, height=1024, total_step=20, direction='h'):
+    base_gg = BASE_RGB_8BIT[1]
+    rr = BASE_RGB_8BIT[0]
+    bb = BASE_RGB_8BIT[2]
+
+    gg_min = base_gg - (total_step // 2)
+    gg_max = base_gg + (total_step // 2)
+
+    if direction == 'h':
+        patch_len = width
+    else:
+        patch_len = height
+
+    gg_grad = np.linspace(gg_min, gg_max, patch_len)
+    rr_static = np.ones_like(gg_grad) * rr
+    bb_static = np.ones_like(gg_grad) * bb
+    line = np.dstack((rr_static, gg_grad, bb_static))
+
+    if direction == 'h':
+        img_base_8bit_float = line * np.ones((height, 1, 3))
+    else:
+        line = line.reshape((height, 1, 3))
+        img_base_8bit_float = line * np.ones((1, width, 3))
+
+    img_out_float_8bit = img_base_8bit_float / 255
+    img_out_8bit = np.round(img_out_float_8bit * 255) / 255
+    img_out_10bit = np.round(img_out_float_8bit * 1023) / 1023
+
+    name_8bit = f"./img/8bit_grad_{width}x{height}_dir_{direction}.png"
+    name_10bit = f"./img/10bit_grad_{width}x{height}_dir_{direction}.png"
+    tpg.img_wirte_float_as_16bit_int(name_8bit, img_out_8bit)
+    tpg.img_wirte_float_as_16bit_int(name_10bit, img_out_10bit)
+
+
+def main_func():
+    create_8bit_10bit_patch(
+        width=512, height=1024, total_step=20, direction='h')
+    create_8bit_10bit_patch(
+        width=1024, height=512, total_step=20, direction='v')
+
+
+if __name__ == '__main__':
+    os.chdir(os.path.dirname(os.path.abspath(__file__)))
+    main_func()
