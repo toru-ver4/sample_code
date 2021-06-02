08b6bc10d8c24b125ae7b68a688af1eb17843487
Toru Yoshihara <toru.ver.11@gmail.com>
2021-06-02 19:35:06 +0900

    #151 plot 1st az-bz plane.

--- 2021/10_study_jzazbz/plot_gamut_boundary_without_lut.py
+++ 2021/10_study_jzazbz/plot_gamut_boundary_without_lut.py
@@ -8,9 +8,15 @@ import os
 
 # import third-party libraries
 import numpy as np
+from colour import XYZ_to_RGB, RGB_COLOURSPACES
 
 # import my libraries
 import color_space as cs
+import transfer_functions as tf
+from jzazbz import jzazbz_to_large_xyz, st2084_oetf_like, st2084_eotf_like
+from create_gamut_booundary_lut import is_out_of_gamut_rgb
+from test_pattern_generator2 import img_wirte_float_as_16bit_int
+import plot_utility as pu
 
 # information
 __author__ = 'Toru Yoshihara'
@@ -22,16 +28,16 @@ __email__ = 'toru.ver.11 at-sign gmail.com'
 __all__ = []
 
 
-def create_valid_ab_plane_image_srgb(
-        l_val=50, ab_max=200, ab_sample=512, color_space_name=cs.BT2020,
-        bg_rgb=np.array([0.5, 0.5, 0.5])):
+def create_valid_ab_plane_image_st2084(
+        j_val=0.5, ab_max=1.0, ab_sample=512, color_space_name=cs.BT2020,
+        bg_rgb_luminance=np.array([50, 50, 50])):
     """
     Create an image that indicates the valid area of the ab plane.
 
     Parameters
     ----------
-    l_val : float
-        A Lightness value. range is 0.0 - 100.0
+    j_val : float
+        A Lightness value. range is 0.0 - 1.0
     ab_max : float
         A maximum value of the a, b range.
     ab_sapmle : int
@@ -39,22 +45,66 @@ def create_valid_ab_plane_image_srgb(
     color_space_name : str
         color space name for colour.RGB_COLOURSPACES
     """
-    dummy_rgb = bg_rgb
     aa_base = np.linspace(-ab_max, ab_max, ab_sample)
     bb_base = np.linspace(-ab_max, ab_max, ab_sample)
     aa = aa_base.reshape((1, ab_sample))\
         * np.ones_like(bb_base).reshape((ab_sample, 1))
     bb = bb_base.reshape((ab_sample, 1))\
         * np.ones_like(aa_base).reshape((1, ab_sample))
-    ll = np.ones_like(aa) * l_val
-    lab = np.dstack((ll, aa, bb[::-1])).reshape((ab_sample, ab_sample, 3))
-    rgb = cs.lab_to_rgb(lab=lab, color_space_name=color_space_name)
-    ng_idx = cgb.is_out_of_gamut_rgb(rgb=rgb)
-    rgb[ng_idx] = dummy_rgb
-    srgb_image = tf.oetf(np.clip(rgb, 0.0, 1.0), tf.SRGB)
+    jj = np.ones_like(aa) * j_val
+    jzazbz = np.dstack((jj, aa, bb[::-1])).reshape((ab_sample, ab_sample, 3))
+    large_xyz = jzazbz_to_large_xyz(jzazbz)
+    rgb_luminance = XYZ_to_RGB(
+        large_xyz, cs.D65, cs.D65,
+        RGB_COLOURSPACES[color_space_name].matrix_XYZ_to_RGB)
 
-    return srgb_image
+    ng_idx = is_out_of_gamut_rgb(rgb=rgb_luminance/10000)
+    rgb_luminance[ng_idx] = bg_rgb_luminance
+    rgb_st2084 = tf.oetf_from_luminance(
+        np.clip(rgb_luminance, 0.0, 10000), tf.ST2084)
+
+    return rgb_st2084
+
+
+def plot_ab_plane_st2084(
+        j_val=0.5, ab_max=1.0, ab_sample=512, color_space_name=cs.BT2020):
+    rgb_st2084 = create_valid_ab_plane_image_st2084(
+        j_val=j_val, ab_max=ab_max, ab_sample=ab_sample,
+        color_space_name=color_space_name)
+    luminance = int(round(st2084_eotf_like(j_val) + 0.5))
+
+    fig, ax1 = pu.plot_1_graph(
+        fontsize=20,
+        figsize=(16, 16),
+        bg_color=(0.96, 0.96, 0.96),
+        graph_title=f"Jz={j_val:.2f},  Luminance={luminance} [cd/m2]?",
+        graph_title_size=None,
+        xlabel="az", ylabel="bz",
+        axis_label_size=None,
+        legend_size=17,
+        xlim=[-ab_max, ab_max],
+        ylim=[-ab_max, ab_max],
+        xtick=None,
+        ytick=None,
+        xtick_size=None, ytick_size=None,
+        linewidth=3,
+        minor_xtick_num=None,
+        minor_ytick_num=None)
+    ax1.plot([0], [0], '.')
+    ax1.imshow(
+        rgb_st2084, extent=(-ab_max, ab_max, -ab_max, ab_max), aspect='auto')
+    pu.show_and_save(
+        fig=fig, legend_loc=None, show=False,
+        save_fname=f"./img/test_ab_j_val-{j_val}.png")
 
 
 if __name__ == '__main__':
     os.chdir(os.path.dirname(os.path.abspath(__file__)))
+    luminance = 100
+    j_val = st2084_oetf_like(luminance)
+    # img = create_valid_ab_plane_image_st2084(
+    #     j_val=j_val, ab_max=0.5, ab_sample=1024, color_space_name=cs.BT2020,
+    #     bg_rgb_luminance=np.array([20, 20, 20]))
+    # img_wirte_float_as_16bit_int(f"./test_lum-{luminance}.png", img)
+    plot_ab_plane_st2084(
+        j_val=j_val, ab_max=0.5, ab_sample=1024, color_space_name=cs.BT2020)
