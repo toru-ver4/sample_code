c383dc093d9af3c511ccc784314f97050cb27ab3
Toru Yoshihara <toru.ver.11@gmail.com>
2021-06-06 23:17:00 +0900

    #151 plot graph for blog.

---
+++ 2021/10_study_jzazbz/make_agif.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+
+cp ./img/prime_on_lab_original.png ./img/mgif/eq05_00.png
+cp ./img/prime_on_lab_original.png ./img/mgif/eq08_00.png
+cp ./img/prime_on_lab_original.png ./img/mgif/eq058_00.png
+
+cp ./img/prime_on_lab_eq5.png ./img/mgif/eq05_01.png
+cp ./img/prime_on_lab_eq5.png ./img/mgif/eq58_00.png
+cp ./img/prime_on_lab_eq5.png ./img/mgif/eq058_01.png
+
+cp ./img/prime_on_lab_eq8.png ./img/mgif/eq08_01.png
+cp ./img/prime_on_lab_eq8.png ./img/mgif/eq58_01.png
+cp ./img/prime_on_lab_eq8.png ./img/mgif/eq058_02.png
+
+
+ffmpeg -r 2 -i ./img/mgif/eq05_%02d.png -filter_complex "split[a][b];[a]palettegen=stats_mode=single[pal];[b][pal]paletteuse=new=1" ./img/mgif/agif_eq05.gif -y
+ffmpeg -r 2 -i ./img/mgif/eq08_%02d.png -filter_complex "split[a][b];[a]palettegen=stats_mode=single[pal];[b][pal]paletteuse=new=1" ./img/mgif/agif_eq08.gif -y
+ffmpeg -r 2 -i ./img/mgif/eq58_%02d.png -filter_complex "split[a][b];[a]palettegen=stats_mode=single[pal];[b][pal]paletteuse=new=1" ./img/mgif/agif_eq58.gif -y
+ffmpeg -r 2 -i ./img/mgif/eq058_%02d.png -filter_complex "split[a][b];[a]palettegen=stats_mode=single[pal];[b][pal]paletteuse=new=1" ./img/mgif/agif_eq058.gif -y
--- 2021/10_study_jzazbz/plot_gamut_boundary_without_lut.py
+++ 2021/10_study_jzazbz/plot_gamut_boundary_without_lut.py
@@ -8,13 +8,13 @@ import os
 
 # import third-party libraries
 import numpy as np
-from colour import XYZ_to_RGB, RGB_COLOURSPACES
+from colour import XYZ_to_RGB, RGB_COLOURSPACES, xy_to_XYZ
 from multiprocessing import Pool, cpu_count
 
 # import my libraries
 import color_space as cs
 import transfer_functions as tf
-from jzazbz import jzazbz_to_large_xyz, st2084_oetf_like, st2084_eotf_like,\
+from jzazbz import jzazbz_to_large_xyz, large_xyz_to_jzazbz, st2084_oetf_like, st2084_eotf_like,\
     jzczhz_to_jzazbz
 from create_gamut_booundary_lut import is_out_of_gamut_rgb
 from test_pattern_generator2 import img_wirte_float_as_16bit_int
@@ -70,7 +70,8 @@ def create_valid_ab_plane_image_st2084(
 
 def create_valid_cj_plane_image_st2084(
         h_val=50, c_max=1, c_sample=1024, j_sample=1024,
-        color_space_name=cs.BT2020, bg_rgb_luminance=np.array([50, 50, 50])):
+        color_space_name=cs.BT2020, bg_rgb_luminance=np.array([50, 50, 50]),
+        maximum_luminance=10000):
     """
     Create an image that indicates the valid area of the ab plane.
 
@@ -88,6 +89,8 @@ def create_valid_cj_plane_image_st2084(
         color space name for colour.RGB_COLOURSPACES
     bg_lightness : float
         background lightness value.
+    maximum_luminance : float
+        maximum luminance of the target display device.
     """
     l_max = 1
 
@@ -105,7 +108,8 @@ def create_valid_cj_plane_image_st2084(
     rgb_luminance = XYZ_to_RGB(
         large_xyz, cs.D65, cs.D65,
         RGB_COLOURSPACES[color_space_name].matrix_XYZ_to_RGB)
-    ng_idx = is_out_of_gamut_rgb(rgb=rgb_luminance/10000)
+    print(f"maximum_luminance = {maximum_luminance}")
+    ng_idx = is_out_of_gamut_rgb(rgb=rgb_luminance/maximum_luminance)
 
     rgb_luminance[ng_idx] = bg_rgb_luminance
 
@@ -116,7 +120,7 @@ def create_valid_cj_plane_image_st2084(
 
 
 def plot_czjz_plane_st2084(
-        h_idx, h_val, color_space_name):
+        h_idx, h_val, color_space_name, maximum_luminance=10000):
     """
     Parameters
     ----------
@@ -126,6 +130,8 @@ def plot_czjz_plane_st2084(
         Hue Value of the ab plane
     color_space_name : str
         color space name for colour.RGB_COLOURSPACES
+    maximum_luminance : float
+        maximum luminance of the target display device.
     """
     c_max = 0.5
     c_sample = 1024
@@ -134,13 +140,14 @@ def plot_czjz_plane_st2084(
     rgb_st2084 = create_valid_cj_plane_image_st2084(
         h_val=h_val, c_max=c_max, c_sample=c_sample, j_sample=j_sample,
         color_space_name=color_space_name,
-        bg_rgb_luminance=np.array([50, 50, 50]))
+        bg_rgb_luminance=np.array([50, 50, 50]),
+        maximum_luminance=maximum_luminance)
 
     fig, ax1 = pu.plot_1_graph(
         fontsize=20,
         figsize=(12, 12),
         bg_color=None,
-        graph_title=f"Cz-Jz Plane, hz={h_val:.1f}°",
+        graph_title=f"Cz-Jz Plane, {maximum_luminance} nits, hz={h_val:.1f}°",
         graph_title_size=None,
         xlabel="Cz", ylabel="Jz",
         axis_label_size=None,
@@ -156,7 +163,7 @@ def plot_czjz_plane_st2084(
     ax1.imshow(
         rgb_st2084, extent=(0, c_max, 0, j_max), aspect='auto')
     prefix = "/work/overuse/2021/10_jzazbz/img_seq_cj"
-    fname = f"{prefix}/cj_plane_{h_idx:04d}.png"
+    fname = f"{prefix}/cj_plane_max-{maximum_luminance}nits_{h_idx:04d}.png"
     print(f"save file = {fname}")
     pu.show_and_save(
         fig=fig, legend_loc=None, show=False, save_fname=fname)
@@ -238,8 +245,8 @@ def thread_wrapper_plot_czjz_plane_st2084(args):
     plot_czjz_plane_st2084(**args)
 
 
-def plot_cj_plane_seq(color_space_name):
-    h_num = 721
+def plot_cj_plane_seq(color_space_name, maximum_luminance=10000):
+    h_num = 361
 
     total_process_num = h_num
     block_process_num = cpu_count() // 2
@@ -254,7 +261,8 @@ def plot_cj_plane_seq(color_space_name):
                 break
             d = dict(
                 h_idx=h_idx, h_val=h_idx/(h_num-1)*360,
-                color_space_name=color_space_name)
+                color_space_name=color_space_name,
+                maximum_luminance=maximum_luminance)
             # plot_czjz_plane_st2084(**d)
             args.append(d)
         #     break
@@ -263,29 +271,46 @@ def plot_cj_plane_seq(color_space_name):
             pool.map(thread_wrapper_plot_czjz_plane_st2084, args)
 
 
+def check_jz_val_for_luminance():
+    luminance_num = 1024
+    x = np.linspace(1/255, 1, luminance_num)
+    luminance = tf.eotf_to_luminance(x, tf.ST2084).reshape(-1, 1)
+    d65_xyz = xy_to_XYZ(cs.D65).reshape(1, 3).repeat(
+        luminance_num, axis=0) * luminance
+    jzazbz = large_xyz_to_jzazbz(xyz=d65_xyz)
+    jz = jzazbz[..., 0]
+
+    fig, ax1 = pu.plot_1_graph(
+        fontsize=20,
+        figsize=(10, 7),
+        bg_color=(0.96, 0.96, 0.96),
+        graph_title="Relationship between Luminance and Jz (D65)",
+        graph_title_size=None,
+        xlabel="Luminance [nits]", ylabel="Jz",
+        axis_label_size=None,
+        legend_size=17,
+        xlim=None,
+        ylim=None,
+        xtick=None,
+        ytick=None,
+        xtick_size=None, ytick_size=None,
+        linewidth=3,
+        minor_xtick_num=None,
+        minor_ytick_num=None)
+    ax1.set_xscale('log', base=10)
+    ax1.plot(luminance, jz, color=(0.2, 0.2, 0.2))
+    fname = "./img/lumi_vs_jz.png"
+    pu.show_and_save(
+        fig=fig, legend_loc=None, show=False, save_fname=fname)
+
+
 if __name__ == '__main__':
     os.chdir(os.path.dirname(os.path.abspath(__file__)))
-    luminance = 100
-    j_val = st2084_oetf_like(luminance)
-    # img = create_valid_ab_plane_image_st2084(
-    #     j_val=j_val, ab_max=0.5, ab_sample=1024, color_space_name=cs.BT2020,
-    #     bg_rgb_luminance=np.array([20, 20, 20]))
-    # img_wirte_float_as_16bit_int(f"./test_lum-{luminance}.png", img)
-
-    # plot_ab_plane_st2084(
-    #     j_idx=0, j_val=j_val, ab_max=0.5, ab_sample=1024,
-    #     color_space_name=cs.BT2020)
+    luminance = 1000
+    # check_iz_jz()
 
     # plot_ab_plane_seq(color_space_name=cs.BT2020)
-
-    # h_val = 0
-    # for h_idx, h_val in enumerate(np.linspace(0, 360, 8, endpoint=False)):
-    #     # img = create_valid_cj_plane_image_st2084(
-    #     #     h_val=h_val, c_max=0.5, c_sample=1024, j_sample=1024,
-    #     #     color_space_name=cs.BT2020,
-    #     #     bg_rgb_luminance=np.array([50, 50, 50]))
-    #     # img_wirte_float_as_16bit_int(f"./img/test_czjz-{h_val}.png", img)
-    #     plot_czjz_plane_st2084(
-    #         h_idx=h_idx, h_val=h_val, color_space_name=cs.BT2020)
-
-    plot_cj_plane_seq(color_space_name=cs.BT2020)
+    # plot_cj_plane_seq(color_space_name=cs.BT2020, maximum_luminance=100)
+    # plot_cj_plane_seq(color_space_name=cs.BT2020, maximum_luminance=1000)
+    # plot_cj_plane_seq(color_space_name=cs.BT2020, maximum_luminance=4000)
+    # plot_cj_plane_seq(color_space_name=cs.BT2020, maximum_luminance=10000)
