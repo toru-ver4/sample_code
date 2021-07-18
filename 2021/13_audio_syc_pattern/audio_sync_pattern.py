c946bfa827683cb194c942548224de67ab46fb58
Toru Yoshihara <toru.ver.11@gmail.com>
2021-07-18 22:58:39 +0900

    #157 create 1st sample

--- 2021/13_audio_syc_pattern/audio_sync_pattern.py
+++ 2021/13_audio_syc_pattern/audio_sync_pattern.py
@@ -47,7 +47,7 @@ def create_pos_list(
 
     # calc idx=0 position
     st_pos_h = (width // 2) - block_width // 2
-    st_pos_v = height - block_height * 4
+    st_pos_v = int(height - block_height * 2)
     base_st_pos = np.array([st_pos_h, st_pos_v], dtype=np.uint16)
     st_pos_list = np.array([base_st_pos], dtype=np.uint16)
     frame_maker_temp = FrameMarker(st_pos_list=st_pos_list)
@@ -88,10 +88,10 @@ def create_fill_block_sequence(
         width=1920, height=1080, fps=60, frame_marker_list=None,
         block_width=100, block_height=100):
 
-    base_img = np.ones((height, width, 3)) * tf.eotf(0.5, tf.GAMMA24)
-    fg_color = tf.eotf(np.array([0.9, 0.9, 0.9]), tf.GAMMA24)
+    base_img = np.zeros((height, width, 3))
+    fg_color = tf.eotf(np.array([192, 192, 192]) / 255, tf.GAMMA24)
     center_color = tf.eotf(np.array([174, 194, 238]) / 255, tf.GAMMA24)
-    bg_color = tf.eotf(np.array([0.1, 0.1, 0.1]), tf.GAMMA24)
+    bg_color = tf.eotf(np.array([36, 36, 36]) / 255, tf.GAMMA24)
     base_dir = "/work/overuse/2021/13_audio_sync_pattern/img_seq/"
     fname_base = base_dir + "sync_{width}x{height}_{fps}p_{idx:04d}.png"
 
@@ -104,7 +104,7 @@ def create_fill_block_sequence(
             target_img=base_img, width=block_width, height=block_height,
             color=color)
 
-    sec = 2
+    sec = 3
     frame = fps * sec
     for idx in range(frame):
         img = base_img.copy()
@@ -112,6 +112,8 @@ def create_fill_block_sequence(
             target_img=img, width=block_width, height=block_height,
             color=fg_color)
         out_img = tf.oetf(np.clip(img, 0, 1), tf.GAMMA24)
+        alpha = create_alpha_channel(out_img)
+        out_img = np.dstack([out_img, alpha])
         fname = fname_base.format(
             width=width, height=height, fps=fps, idx=idx)
         print(fname)
@@ -123,7 +125,7 @@ def main_func(fps=60):
     height = 1080
     padding = int(round(width * 0.05)) & 0xFFFC
     block_width = int(((width - padding * 2) / fps)) // 2
-    block_height = int(round(height * 0.1)) & 0xFFFC
+    block_height = int(round(height * 0.05)) & 0xFFFC
     margin = block_width
 
     frame_marker_list = create_pos_list(
@@ -136,9 +138,30 @@ def main_func(fps=60):
         block_width=block_width, block_height=block_height)
 
 
+def create_alpha_channel(img):
+    alpha_img = np.zeros((img.shape[0], img.shape[1], 1))
+    non_zero_idx = ((img[..., 0] > 0) | (img[..., 1] > 0)) | (img[..., 2] > 0)
+    alpha_img[non_zero_idx] = 1
+
+    return alpha_img
+
+
+def debug_alpha_blend():
+    img = np.zeros((1080, 1920, 3))
+    color = np.array([0.5, 0.5, 0.5])
+    img[300:600, 300:600] = np.ones((300, 300, 3)) * color
+
+    alpha_img = create_alpha_channel(img)
+
+    img_with_alpha = np.dstack([img, alpha_img])
+
+    tpg.img_wirte_float_as_16bit_int("./img/debug_alpha.png", img_with_alpha)
+
+
 if __name__ == '__main__':
     os.chdir(os.path.dirname(os.path.abspath(__file__)))
-    main_func(fps=24)
-    main_func(fps=30)
-    main_func(fps=50)
+    # main_func(fps=24)
+    # main_func(fps=30)
+    # main_func(fps=50)
     main_func(fps=60)
+    # debug_alpha_blend()
