b1ff822aae90ee149509f199544d09f4b8845af5
Toru Yoshihara <toru.ver.11@gmail.com>
2021-08-04 19:52:03 +0900

    #160 implemented tiled pattern.

--- 2021/14_optical_illusion/optical_illusion.py
+++ 2021/14_optical_illusion/optical_illusion.py
@@ -7,16 +7,21 @@
 # import standard libraries
 import os
 from multiprocessing import Pool, cpu_count
+from typing import NamedTuple
 
 # import third-party libraries
 import numpy as np
 import cv2
+from colour import LCHab_to_Lab
 from colour.utilities import tstack
+from numpy.core.shape_base import stack
+from numpy.random import sample
 
 # import my libraries
 import test_pattern_generator2 as tpg
 import transfer_functions as tf
 import color_space as cs
+from common import MeasureExecTime
 
 # information
 __author__ = 'Toru Yoshihara'
@@ -28,22 +33,60 @@ __email__ = 'toru.ver.11 at-sign gmail.com'
 __all__ = []
 
 lightness = 70
-bbb = 50
-bg_lab = np.array([lightness, 42, 18])
-fg_lab = np.array([lightness, 0, -50])
+base_chroma = 50
+base_hue = 270
 
-bg_color = cs.lab_to_rgb(bg_lab, cs.BT709)
+edge_lch = np.array([lightness, base_chroma, base_hue])
+edge_lab = LCHab_to_Lab(edge_lch)
+fg_lab = np.array([lightness, 0, 50])
+
+base_value = 0.0
 fg_color = cs.lab_to_rgb(fg_lab, cs.BT709)
+edge_color = cs.lab_to_rgb(edge_lab, cs.BT709)
 
-bg_color[bg_color <= 0] = 0.01
-fg_color[fg_color <= 0] = 0.01
+fg_color[fg_color <= 0] = 0.001
+edge_color[edge_color <= 0] = 0.001
+
+
+class BaseParam(NamedTuple):
+    amp: int = 200
+    dot_pattern_rate: int = 16
+    radius_out: int = 250
+    radius_in: int = 150
+    dst_width: int = 1000
+    dst_height: int = 1000
+    width: int = 2000
+    height: int = 2000
+    fps: int = 60
+    cycle_num: int = 6
+    cycle_sec: float = 0.8
+    cycle_sec_offset: float = 0.0
+    amp_offset: int = 0
+    tile_v_num: int = 4
+    tile_h_num: int = 2
+
+
+def create_fg_color_list(sample_num):
+    st_hue = base_hue + 90
+    ed_hue = st_hue + 180
+    hh = np.linspace(st_hue, ed_hue, sample_num)
+    ll = np.ones_like(hh) * lightness
+    cc = np.ones_like(hh) * base_chroma
+    lch = tstack([ll, cc, hh])
+    lab = LCHab_to_Lab(lch)
+    rgb = cs.lab_to_rgb(lab, cs.BT709)
+    rgb = np.clip(rgb, 0.0, 1.0)
+
+    return rgb
 
 
 def create_bg_dot_pattern(
         width=640, height=640, dot_pattern_rate=16,
-        radius_out=250, radius_in=150):
+        radius_out=250, radius_in=150,
+        fg_color=fg_color, edge_color=edge_color):
     dot_img_width = width // dot_pattern_rate
     dot_img_height = height // dot_pattern_rate
+    np.random.seed(100)
 
     mask = np.random.randint(0, 2, (dot_img_height, dot_img_width, 1))
     inv_mask = np.uint8(1 - mask)
@@ -53,15 +96,13 @@ def create_bg_dot_pattern(
         dot_img, (width, height), interpolation=cv2.INTER_NEAREST)
 
     center = (width//2, height//2)
-    radius = radius_out
 
-    img = img * bg_color
-    # img[img <= 0] = 1.0
+    img = img * fg_color
+    img[img <= 0] = base_value
     img[inv_mask] = np.array([[[1.0, 1.0, 1.0]]])
-    img = cv2.circle(img, center, radius, bg_color, -1)
+    img = cv2.circle(img, center, radius_out, fg_color, -1)
 
-    radius2 = radius_in
-    img = cv2.circle(img, center, radius2, fg_color, -1)
+    img = cv2.circle(img, center, radius_in, edge_color, -1)
 
     img_non_linear = np.uint16(tf.oetf(np.clip(img, 0, 1), tf.SRGB) * 0xFFFF)
 
@@ -112,7 +153,7 @@ def thread_wrapper_create_move_seq_core(args):
     create_move_seq_core(**args)
 
 
-def create_move_seq():
+def create_move_seq_1st_sample():
     amp = 200
     dot_pattern_rate = 16
     radius_out = 250
@@ -146,6 +187,9 @@ def create_move_seq():
     block_process_num = int(cpu_count() / 2 + 0.999)
     block_num = int(round(total_process_num / block_process_num + 0.5))
 
+    mt = MeasureExecTime()
+
+    mt.start()
     for b_idx in range(block_num):
         args = []
         for p_idx in range(block_process_num):
@@ -157,15 +201,129 @@ def create_move_seq():
                 base_img=base_img,
                 dst_width=dst_width, dst_height=dst_height,
                 pos_list=pos_list, idx=j_idx)
-            # create_move_seq_core(**d)
+            create_move_seq_core(**d)
+            mt.lap()
             # print(d)
+            args.append(d)
+            # break
+        # break
+        # with Pool(block_process_num) as pool:
+        #     pool.map(thread_wrapper_create_move_seq_core, args)
+    mt.end()
+
+
+def trim_image(base_img, idx, pos_list, bp: BaseParam):
+    v_st = pos_list[idx, 1]
+    v_ed = v_st + bp.dst_height
+    h_st = pos_list[idx, 0]
+    h_ed = h_st + bp.dst_width
+
+    dst_img = base_img[v_st:v_ed, h_st:h_ed]
+
+    return dst_img
+
+
+def create_multi_move_pattern_core(
+        f_idx, bp: BaseParam, fg_color_list, pos_list):
+    v_img_buf = []
+    for v_idx in range(bp.tile_v_num):
+        h_img_buf = []
+        for h_idx in range(bp.tile_h_num):
+            c_idx = v_idx * bp.tile_h_num + h_idx
+            base_img = create_bg_dot_pattern(
+                width=bp.width, height=bp.height,
+                dot_pattern_rate=bp.dot_pattern_rate,
+                radius_out=bp.radius_out, radius_in=bp.radius_in,
+                fg_color=fg_color_list[c_idx], edge_color=edge_color)
+            img = trim_image(
+                base_img=base_img, idx=f_idx, pos_list=pos_list, bp=bp)
+
+            h_img_buf.append(img)
+        v_img_buf.append(np.hstack(h_img_buf))
+
+    out_img = np.vstack(v_img_buf)
+
+    base_dir = "/work/overuse/2021/14_optical_illusion/multi_move_sample/"
+    fname_base = base_dir + "multi_test_seq_{idx:04d}.png"
+    fname = fname_base.format(idx=f_idx)
+    print(fname)
+    tpg.img_write(fname, out_img)
+
+
+def calc_pos_list(bp: BaseParam):
+    offset_list = create_horizontal_movement(
+        fps=bp.fps, cycle_num=bp.cycle_num, cycle_sec=bp.cycle_sec,
+        cycle_sec_offset=bp.cycle_sec_offset, amp=bp.amp,
+        amp_offset=bp.amp_offset)
+    h_pos_list = np.uint16(
+        np.round((bp.width // 2) - offset_list - (bp.dst_width // 2)))
+    v_pos_list = np.ones_like(h_pos_list)\
+        * ((bp.height // 2) - (bp.dst_height // 2))
+    pos_list = np.uint16(tstack([h_pos_list, v_pos_list]))
+
+    return pos_list
+
+
+def create_multi_color_movie_sample():
+    g_bg_width = 3840
+    g_bg_height = 2160
+
+    bp = BaseParam(
+        amp=g_bg_width//26,
+        dot_pattern_rate=16,
+        radius_out=100,
+        radius_in=70,
+        dst_width=g_bg_width//8,
+        dst_height=g_bg_height//4,
+        width=g_bg_width//4,
+        height=g_bg_height//2,
+        fps=60,
+        cycle_num=6,
+        cycle_sec=0.8,
+        cycle_sec_offset=0.0,
+        amp_offset=0,
+        tile_v_num=4,
+        tile_h_num=8,
+    )
+    sample_color_num = bp.tile_v_num * bp.tile_h_num
+    fg_color_list = create_fg_color_list(sample_num=sample_color_num)
+    pos_list = calc_pos_list(bp=bp)
+
+    frame_num = len(pos_list)
+
+    total_process_num = frame_num
+    block_process_num = int(cpu_count())
+    block_num = int(round(total_process_num / block_process_num + 0.5))
+
+    mt = MeasureExecTime()
+    mt.start()
+    for b_idx in range(block_num):
+        args = []
+        for p_idx in range(block_process_num):
+            f_idx = b_idx * block_process_num + p_idx              # User
+            print(f"b_idx={b_idx}, p_idx={p_idx}, f_idx={f_idx}")  # User
+            if f_idx >= total_process_num:                         # User
+                break
+            d = dict(
+                f_idx=f_idx, bp=bp,
+                fg_color_list=fg_color_list, pos_list=pos_list)
+            # print(d)
+            # create_multi_move_pattern_core(**d)
             args.append(d)
         #     break
         # break
         with Pool(block_process_num) as pool:
-            pool.map(thread_wrapper_create_move_seq_core, args)
+            pool.map(thread_wrapper_create_multi_move_pattern_core, args)
+        mt.lap()
+    mt.end()
+
+
+def thread_wrapper_create_multi_move_pattern_core(args):
+    create_multi_move_pattern_core(**args)
 
 
 if __name__ == '__main__':
     os.chdir(os.path.dirname(os.path.abspath(__file__)))
-    create_move_seq()
+    # create_move_seq_1st_sample()
+    # create_fg_color_list()
+    create_multi_color_movie_sample()
