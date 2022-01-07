33673c36035020b59ac843f7fb2c913590b5edd6
Toru Yoshihara <toru.ver.11@gmail.com>
2022-01-08 00:11:07 +0900

    #174 under research

---
+++ 2022/01_OCIO_V2_sample/dump_builtin_transforms.py
@@ -0,0 +1,29 @@
+# -*- coding: utf-8 -*-
+"""
+dump
+"""
+
+# import standard libraries
+import os
+
+# import third-party libraries
+import PyOpenColorIO as ocio
+
+# import my libraries
+
+# information
+__author__ = 'Toru Yoshihara'
+__copyright__ = 'Copyright (C) 2022 - Toru Yoshihara'
+__license__ = 'New BSD License - https://opensource.org/licenses/BSD-3-Clause'
+__maintainer__ = 'Toru Yoshihara'
+__email__ = 'toru.ver.11 at-sign gmail.com'
+
+__all__ = []
+
+
+if __name__ == '__main__':
+    os.chdir(os.path.dirname(os.path.abspath(__file__)))
+    builtin_transform_registry = ocio.BuiltinTransformRegistry()
+    builtin_transforms = builtin_transform_registry.getBuiltins()
+    for builtin_transform in builtin_transforms:
+        print(builtin_transform)
