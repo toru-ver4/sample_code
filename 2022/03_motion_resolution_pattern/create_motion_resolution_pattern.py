2647b2814646dacd40ff8218bb899d00c8f72027
Toru Yoshihara <toru.ver.11@gmail.com>
2022-04-17 11:49:17 +0900

    #180 created LineUnitBlock.

--- 2022/03_motion_resolution_pattern/create_motion_resolution_pattern.py
+++ 2022/03_motion_resolution_pattern/create_motion_resolution_pattern.py
@@ -29,6 +29,14 @@ __all__ = []
 FFMPEG_NORMALIZE_COEF = 65340
 FFMPEG_NORMALIZE_COEF_INV = 65535/65340
 
+IDMS_LINE_CV_LIST = [
+    0, 128, 192, 0, 96, 144, 0, 64, 96]
+IDMS_LINE_COLOR_LIST\
+    = np.repeat(IDMS_LINE_CV_LIST, 3).reshape(len(IDMS_LINE_CV_LIST), 3)
+
+IDMS_BG_CV_LIST = [255, 192, 128]
+IDMS_BG_COLOR_LIST = np.repeat(IDMS_BG_CV_LIST, 3*3).reshape(-1, 3)
+
 
 def conv_Nbit_to_linear(x, bit_depth=8, tf_str=tf.GAMMA24):
     """
@@ -152,7 +160,6 @@ class LineUnit():
         self.line_color2 = line_color2
         self.line_color_num = 2  # color1 and color2
         self.bg_color = bg_color
-        self.init_parameter_check()
 
         self.create_bg_image()
         self.num_of_lines = num_of_lines
@@ -165,14 +172,6 @@ class LineUnit():
         line_st_pos_h = self.width//2 - line_area_width//2
         line_st_pos_v = 0
         self.line_st_pos = [line_st_pos_h, line_st_pos_v]
-
-    def init_parameter_check(self):
-        if len(self.line_color1) != len(self.line_color2):
-            msg = "line_color1 and line_color2 is not same lnegth."
-            raise ValueError(msg)
-        if len(self.line_color1) != len(self.bg_color):
-            msg = "line_color1 and bg_color is not same lnegth."
-            raise ValueError(msg)
 
     def create_bg_image(self):
         self.img = np.ones((self.height, self.width, 3)) * self.bg_color
@@ -193,6 +192,68 @@ class LineUnit():
             self.img[st_pos_v:ed_pos_v, st_pos_h:ed_pos_h] = self.line_color2
 
 
+class LineUnitBlock():
+    def __init__(
+            self, width, height, line_width, num_of_lines,
+            line_color1_list, line_color2_list, bg_color_list):
+        """
+        Parameters
+        ----------
+        width : int
+            width
+        height : int
+            height
+        line_width : int
+            width of the line.
+        num_of_lines : int
+            number of the lines.
+        line_color1_list : ndarray
+            line color value list. It must be linear.
+            (ex. np.array([[0.5, 1.0, 0.9], [1.0, 0.8, 0.7], [1.0, 0.8, 0.7]]))
+        line_color2_list : ndarray
+            line color value list. It must be linear.
+            (ex. np.array([[0.5, 1.0, 0.9], [1.0, 0.8, 0.7], [1.0, 0.8, 0.7]]))
+        bg_color_list : ndarray
+            bg color value list. It must be linear.
+            (ex. np.array([[0.5, 1.0, 0.9], [1.0, 0.8, 0.7], [1.0, 0.8, 0.7]]))
+        """
+        self.width = width
+        self.height = height
+        self.line_width = line_width
+        self.num_of_lines = num_of_lines
+        self.line_color1_list = line_color1_list
+        self.line_color2_list = line_color2_list
+        self.bg_color_list = bg_color_list
+
+        self.init_parameter_check()
+        self.num_of_unit = len(line_color1_list)
+        self.unit_width = self.width
+        self.unit_height_list\
+            = tpg.equal_devision(self.height, self.num_of_unit)
+        self.draw()
+
+    def init_parameter_check(self):
+        if len(self.line_color1_list) != len(self.line_color2_list):
+            msg = "line_color1 and line_color2 is not same lnegth."
+            raise ValueError(msg)
+        if len(self.line_color1_list) != len(self.bg_color_list):
+            msg = "line_color1 and bg_color is not same lnegth."
+            raise ValueError(msg)
+
+    def draw(self):
+        img_v_buf = []
+        for idx in range(self.num_of_unit):
+            lu = LineUnit(
+                width=self.unit_width, height=self.unit_height_list[idx],
+                line_width=self.line_width, num_of_lines=self.num_of_lines,
+                line_color1=self.line_color1_list[idx],
+                line_color2=self.line_color2_list[idx],
+                bg_color=self.bg_color_list[idx])
+            img_v_buf.append(lu.img)
+
+        self.img = np.vstack(img_v_buf)
+
+
 def debug_line_unit():
     width = 640
     height = 360
@@ -211,23 +272,30 @@ def debug_line_unit():
     write_image(out_img/0xFF, "./hoge.png", 'uint8')
 
 
+def debug_line_unit_block():
+    width = 640
+    height = 900
+    line_width = 6
+    num_of_lines = 8
+    line_color1_list = conv_8bit_to_linear(IDMS_LINE_COLOR_LIST)
+    bg_color_list = conv_8bit_to_linear(IDMS_BG_COLOR_LIST)
+    line_color2_list = bg_color_list
+
+    lub = LineUnitBlock(
+            width=width, height=height, line_width=line_width,
+            num_of_lines=num_of_lines,
+            line_color1_list=line_color1_list,
+            line_color2_list=line_color2_list,
+            bg_color_list=bg_color_list)
+    out_img = conv_linear_to_8bit(lub.img)
+    write_image(out_img/0xFF, "./hoge.png", 'uint8')
+
+
 def debug_func():
-    debug_line_unit()
+    # debug_line_unit()
+    debug_line_unit_block()
 
 
 if __name__ == '__main__':
     os.chdir(os.path.dirname(os.path.abspath(__file__)))
     debug_func()
-    # x = [255, 128, 64]
-    # tf_str = tf.ST2084
-    # y = conv_8bit_to_linear(x, tf_str)
-    # print(y)
-    # x2 = conv_linear_to_8bit(y, tf_str)
-    # print(x2)
-
-    # x = [1023, 512, 128]
-    # tf_str = tf.ST2084
-    # y = conv_Nbit_to_linear(x, bit_depth=10, tf_str=tf_str)
-    # print(y)
-    # x2 = conv_linar_to_Nbit(y, bit_depth=10, tf_str=tf_str)
-    # print(x2)
