77128b7c399bda4d69d477a8e8b1d0cffc4c9523
Toru Yoshihara <toru.ver.11@gmail.com>
2022-10-02 19:36:40 +0900

    #197 moved to tpg2.

--- 2022/08_motion_blur_pattern_rev2/debug_scroll_image.py
+++ 2022/08_motion_blur_pattern_rev2/debug_scroll_image.py
@@ -4,7 +4,7 @@
 
 # import standard libraries
 import os
-from turtle import heading
+from multiprocessing import Pool, cpu_count
 
 # import third-party libraries
 import numpy as np
@@ -22,52 +22,6 @@ __email__ = 'toru.ver.11 at-sign gmail.com'
 __all__ = []
 
 
-def scroll_image(img, offset_h, offset_v):
-    """
-    Scroll the input image based on the offset parameters.
-
-    Parameters
-    ----------
-    img : ndarray
-        A image data.
-    offset_h : int
-        A horizontal offset
-    offset_v : int
-        A vertical offset
-
-    Returns
-    -------
-    ndarray
-        A scrolled image data
-    """
-    height, width = img.shape[:2]
-    out_img = np.zeros_like(img)
-    pos_h = offset_h % width
-    pos_v = offset_v % height
-
-    pt1_h = 0
-    pt1_v = 0
-    pt2_h = pos_h
-    pt2_v = 0
-    pt3_h = pos_h
-    pt3_v = pos_v
-    pt4_h = width
-    pt4_v = pos_v
-    pt5_h = 0
-    pt5_v = pos_v
-    pt6_h = pos_h
-    pt6_v = height
-    pt7_h = width
-    pt7_v = height
-
-    out_img[:height-pos_v, :width-pos_h] = img[pt3_v:pt7_v, pt3_h:pt7_h]
-    out_img[:height-pos_v, width-pos_h:] = img[pt5_v:pt6_v, pt5_h:pt6_h]
-    out_img[height-pos_v:, :width-pos_h] = img[pt2_v:pt4_v, pt2_h:pt4_h]
-    out_img[height-pos_v:, width-pos_h:] = img[pt1_v:pt3_v, pt1_h:pt3_h]
-
-    return out_img
-
-
 def debug_func_scroll_image():
     base_img = tpg.img_read("./img/scale_tp_1920x1080.png")
     height, width = base_img.shape[:2]
@@ -77,13 +31,58 @@ def debug_func_scroll_image():
     for idx in range(num_of_step * 2):
         offset_h = step_h * idx
         offset_v = step_v * idx
-        out_img = scroll_image(
+        out_img = tpg.scroll_image(
             img=base_img, offset_h=offset_h, offset_v=offset_v)
         fname = f"./debug/scale_{idx:08d}.png"
         print(fname)
         tpg.img_write(fname, out_img)
 
 
+def thread_wrapper_create_scroll_image_seq_core(args):
+    create_scroll_image_seq_core(**args)
+
+
+def create_scroll_image_seq_core(
+        frame_idx, offset_h, offset_v, in_fname, step_h, step_v):
+    base_img = tpg.img_read(in_fname)
+    out_img = tpg.scroll_image(
+        img=base_img, offset_h=offset_h, offset_v=offset_v)
+    fname = f"./debug/scale_step-hv_{step_h}_{step_v}_{frame_idx:08d}.png"
+    print(fname)
+    tpg.img_write(fname, out_img)
+
+
+def create_scroll_image_seq():
+    in_fname = "./img/scale_tp_3840x2160.png"
+    num_of_frame = 480
+    step_h = 7
+    step_v = -5
+
+    total_process_num = num_of_frame
+    block_process_num = int(cpu_count() * 0.8)
+    block_num = int(round(total_process_num / block_process_num + 0.5))
+
+    for b_idx in range(block_num):
+        args = []
+        for p_idx in range(block_process_num):
+            l_idx = b_idx * block_process_num + p_idx              # User
+            print(f"b_idx={b_idx}, p_idx={p_idx}, l_idx={l_idx}")  # User
+            if l_idx >= total_process_num:                         # User
+                break
+            offset_h = step_h * l_idx
+            offset_v = step_v * l_idx
+            d = dict(
+                frame_idx=l_idx, offset_h=offset_h, offset_v=offset_v,
+                in_fname=in_fname, step_h=step_h, step_v=step_v)
+            args.append(d)
+        #     create_scroll_image_seq_core(**d)
+        #     break
+        # break
+        with Pool(block_process_num) as pool:
+            pool.map(thread_wrapper_create_scroll_image_seq_core, args)
+
+
 if __name__ == '__main__':
     os.chdir(os.path.dirname(os.path.abspath(__file__)))
-    debug_func_scroll_image()
+    # debug_func_scroll_image()
+    create_scroll_image_seq()
--- ty_lib/test_pattern_generator2.py
+++ ty_lib/test_pattern_generator2.py
@@ -2611,6 +2611,52 @@ def create_dot_mesh_image(
     return img
 
 
+def scroll_image(img, offset_h, offset_v):
+    """
+    Scroll the input image based on the offset parameters.
+
+    Parameters
+    ----------
+    img : ndarray
+        A image data.
+    offset_h : int
+        A horizontal offset
+    offset_v : int
+        A vertical offset
+
+    Returns
+    -------
+    ndarray
+        A scrolled image data
+    """
+    height, width = img.shape[:2]
+    out_img = np.zeros_like(img)
+    pos_h = offset_h % width
+    pos_v = offset_v % height
+
+    pt1_h = 0
+    pt1_v = 0
+    pt2_h = pos_h
+    pt2_v = 0
+    pt3_h = pos_h
+    pt3_v = pos_v
+    pt4_h = width
+    pt4_v = pos_v
+    pt5_h = 0
+    pt5_v = pos_v
+    pt6_h = pos_h
+    pt6_v = height
+    pt7_h = width
+    pt7_v = height
+
+    out_img[:height-pos_v, :width-pos_h] = img[pt3_v:pt7_v, pt3_h:pt7_h]
+    out_img[:height-pos_v, width-pos_h:] = img[pt5_v:pt6_v, pt5_h:pt6_h]
+    out_img[height-pos_v:, :width-pos_h] = img[pt2_v:pt4_v, pt2_h:pt4_h]
+    out_img[height-pos_v:, width-pos_h:] = img[pt1_v:pt3_v, pt1_h:pt3_h]
+
+    return out_img
+
+
 if __name__ == '__main__':
     os.chdir(os.path.dirname(os.path.abspath(__file__)))
     # print(calc_rad_patch_idx(outmost_num=9, current_num=1))
