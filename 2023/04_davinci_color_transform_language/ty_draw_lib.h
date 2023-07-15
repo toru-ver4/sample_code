58e9925af645e110e4a4a306001ba73599d0b192
Toru Yoshihara <toru.ver.11@gmail.com>
2023-07-15 16:03:00 +0900

    #217 add scale to dctl (#218)
    * #217 drew vertical rectangle
    
    * #217 rename
    
    * #217 rename
    
    * #217 create int2
    
    * #217 drawing scale
    
    * #217 added text
    
    * #217 added background color for luminance text
    
    * #217 added "show scale" checkbox
    
    * #217 temporary commit
    
    * complete
    
    * #217 modify size

--- 2023/04_davinci_color_transform_language/ty_draw_lib.h
+++ 2023/04_davinci_color_transform_language/ty_draw_lib.h
@@ -1,5 +1,5 @@
-#ifndef SHOW_INTERNAL_VALUE_H
-#define SHOW_INTERNAL_VALUE_H
+#ifndef TY_DRAW_LIB
+#define TY_DRAW_LIB
 
 #define FONT_COLOR_IDX_RED (0)
 #define FONT_COLOR_IDX_GREEN (1)
@@ -16,6 +16,12 @@
 #define CROSS_HAIR_COLOR_IDX_YELLOW (5)
 
 
+typedef struct{
+    int x;
+    int y;
+} int2;
+
+
 __CONSTANT__ float3 rgbmyc_color[] = {
     {0.5, 0.0, 0.0},
     {0.0, 0.5, 0.0},
@@ -28,10 +34,26 @@ __CONSTANT__ float3 rgbmyc_color[] = {
 __CONSTANT__ float3 cross_hair_color = {0.5, 0.0, 0.5};
 __CONSTANT__ float3 seven_seg_color = {0.5, 0.5, 0.5};
 __CONSTANT__ float3 cross_hair_edge_color = {0.0, 0.0, 0.0};
+__CONSTANT__ float3 black_color = {0.0, 0.0, 0.0};
+__CONSTANT__ float3 grey_color = {0.6, 0.6, 0.6};
 __CONSTANT__ int digit_to_mask[] = {0x3F, 0x06, 0x5B, 0x4F, 0x66, 0x6D, 0x7D, 0x07, 0x7F, 0x6F};
 #define TEXT_PERIOD_MASK (0x80)
 #define TEXT_NEGATIVE_MASK (0x40)
 #define TEXT_EFFECTIVE_DIGIT (6)
+
+
+__DEVICE__ int draw_rectangle_int(int p_Width, int p_Height, int p_X, int p_Y, float3 *rgb, int2 st_pos, int2 ed_pos, float3 *line_color)
+{
+    if((st_pos.x <= p_X) && (p_X < ed_pos.x)){
+        if((st_pos.y <= p_Y) && (p_Y < ed_pos.y)){
+            rgb->x = line_color->x;
+            rgb->y = line_color->y;
+            rgb->z = line_color->z;
+        }
+    }
+
+    return 0;
+}
 
 
 __DEVICE__ int draw_rectangle(int p_Width, int p_Height, int p_X, int p_Y, float3 *rgb, float2 st_pos, float2 ed_pos, float3 *line_color)
@@ -232,6 +254,67 @@ __DEVICE__ float2 calc_text_width(int r_width, int r_height)
     out.y = r_height * 4;            // text width for period
 
     return out;
+}
+
+
+__DEVICE__ float2 draw_digits_int(int p_Width, int p_Height, int p_X, int p_Y, float3 *rgb, float drawing_value, int2 g_st_pos, int r_height, int r_width, float3 *font_color)
+{
+    int ii;
+    float2 text_width_float = calc_text_width(r_width, r_height);
+    int text_width = int(text_width_float.x);
+    int text_width_period = int(text_width_float.y);
+    float magnitude_value;
+    int digit;
+    int integer_digits = calc_integer_digits(drawing_value);
+    int decimal_digits;
+    int drawing_value_int;
+    int is_negative = 0;
+    float2 st_pos;
+    st_pos.x = g_st_pos.x;
+    st_pos.y = g_st_pos.y;
+
+    // draw background
+    int text_height = (r_height + r_width) * 2;
+    int text_height_margin = r_height * 2;
+
+    // draw background (dark)
+    if((p_Y >= (g_st_pos.y - text_height_margin)) && (p_Y < (g_st_pos.y + text_height + text_height_margin))){
+        if((p_X >= (g_st_pos.x - r_height * 2)) && (p_X < (g_st_pos.x + (text_width * integer_digits + text_width) - text_width))){
+            rgb->x = rgb->x / 3.0;
+            rgb->y = rgb->y / 3.0;
+            rgb->z = rgb->z / 3.0;
+        }
+    }
+
+    if(drawing_value < 0){
+        is_negative = 1;
+    }
+    drawing_value = _fabs(drawing_value);
+    // integer_digits = calc_integer_digits(drawing_value);
+    decimal_digits = TEXT_EFFECTIVE_DIGIT - integer_digits;
+    if(is_negative){
+        decimal_digits -= 1;  // Using one digit to render the "-" character.
+    }
+
+    // convert float to int for round-up.
+    drawing_value_int = int(_round(drawing_value * _powf(10, decimal_digits)));
+
+    // for negative value
+    if(is_negative){
+        draw_single_digit(p_Width, p_Height, p_X, p_Y, rgb, st_pos, r_height, r_width, TEXT_NEGATIVE_MASK, font_color);
+        st_pos.x += text_width;
+    }
+
+    // draw integer value
+    st_pos.x -= text_width;  // To neutralize the effect of the initial offset calculation in the for loop.
+    for(ii=0; ii<integer_digits; ii++){
+        magnitude_value = _powf(10, (integer_digits + decimal_digits - ii - 1));
+        digit = int(_fmod((_floorf(drawing_value_int / magnitude_value)), 10));
+        st_pos.x += text_width;
+        draw_single_digit(p_Width, p_Height, p_X, p_Y, rgb, st_pos, r_height, r_width, digit_to_mask[digit], font_color);
+    }
+
+    return st_pos;
 }
 
 
