25e12b32b6e9dfbdfd7b1c4cda60e3798b891c40
Toru Yoshihara <toru.ver.11@gmail.com>
2024-07-15 23:14:21 +0900

    #246 progress report

--- 2024/09_Check_Windows_ISO_HDR_Output/create_avif_patch.py
+++ 2024/09_Check_Windows_ISO_HDR_Output/create_avif_patch.py
@@ -66,7 +66,64 @@ def create_specific_luminance_small_patch(luminance=203, size=256):
     parent = str(pp.parent)
     avif_fname = "./" + parent + "/" + pp.stem + ".avif"
     cmd = [
-        "avifenc", png_fname, "-d", "10", "-y", "444", "--cicp", "9/16/9",
+        "avifenc", png_fname, "-d", "10", "-y", "444", "--cicp", "9/16/0",
+        "-r", "full", "--lossless", "--ignore-exif", avif_fname
+    ]
+    print(" ".join(cmd))
+    subprocess.run(cmd)
+
+
+def create_specific_luminance_small_patch_all():
+    lumiannce_list = [
+        60, 80, 100, 120, 140, 160, 180,
+        203, 220, 240, 260, 280, 300, 320]
+    patch_size = 128
+    for lumiannce in lumiannce_list:
+        create_specific_luminance_small_patch(
+            luminance=lumiannce, size=patch_size
+        )
+
+
+def encode_av1_tp_video(pix_fmt='yuv444p12le'):
+    in_fname = "./debug/src_tp/step_ramp_step_65_wo_alpha.png"
+    out_fname = f"./debug/src_tp/step_ramp_step_65_{pix_fmt}.mp4"
+    fps = 24
+    cmd = "ffmpeg"
+    ops = [
+        '-color_primaries', 'bt2020',
+        '-color_trc', 'smpte2084',
+        '-colorspace', 'bt2020nc',
+        '-loop', '1',
+        '-r', str(fps),
+        '-i', in_fname,
+        "-an",
+        '-t', "10",
+        '-c:v', 'libaom-av1',
+        '-movflags', 'write_colr',
+        '-pix_fmt', pix_fmt,
+        '-crf', '0',
+        '-color_primaries', 'bt2020',
+        '-color_trc', 'smpte2084',
+        '-colorspace', 'bt2020nc',
+        str(out_fname), '-y'
+    ]
+    args = [cmd] + ops
+    print(" ".join(args))
+    subprocess.run(args)
+
+
+def trim_3840x960():
+    in_fname = "./debug/src_tp/SMPTE ST2084_ITU-R BT.2020_D65_3840x2160_rev07_type1.png"
+    crop_fname = "./debug/src_tp/HDR_TP_3840x960.png"
+    img = tpg.img_read(in_fname)
+    img_crop = img[2158-960:2158]
+    tpg.img_write(crop_fname, img_crop)
+
+    pp = Path(crop_fname)
+    parent = str(pp.parent)
+    avif_fname = "./" + parent + "/" + pp.stem + ".avif"
+    cmd = [
+        "avifenc", crop_fname, "-d", "10", "-y", "444", "--cicp", "9/16/0",
         "-r", "full", "--lossless", "--ignore-exif", avif_fname
     ]
     print(" ".join(cmd))
@@ -75,5 +132,7 @@ def create_specific_luminance_small_patch(luminance=203, size=256):
 
 if __name__ == '__main__':
     os.chdir(os.path.dirname(os.path.abspath(__file__)))
-    # create_specific_luminance_small_patch(luminance=1000, size=512)
-    encode_HDR_TP()
+    # create_specific_luminance_small_patch_all()
+    # encode_HDR_TP()
+    # encode_av1_tp_video(pix_fmt="yuv420p10le")
+    trim_3840x960()
