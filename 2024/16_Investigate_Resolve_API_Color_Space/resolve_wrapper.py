5ef434661897b77b75bd878fa3981eb2fc0fa197
Toru Yoshihara <toru.ver.11@gmail.com>
2024-12-07 18:21:17 +0900

    progress report

--- 2024/16_Investigate_Resolve_API_Color_Space/resolve_wrapper.py
+++ 2024/16_Investigate_Resolve_API_Color_Space/resolve_wrapper.py
@@ -2,6 +2,7 @@
 
 import sys
 import os
+import functools
 
 if sys.platform == "darwin":  # macOS
     resolve_script_api = (
@@ -22,56 +23,185 @@ elif sys.platform == "linux":  # Linux
 import DaVinciResolveScript as dvr_script
 
 
-def get_resolve():
-    """
-    Returns
-    -------
-        BlackmagicFusion.PyRemoteObject
-            The Resolve instance
-    """
-    resolve = dvr_script.scriptapp("Resolve")
+resolve = dvr_script.scriptapp("Resolve")
+if resolve is None:
+    raise ConnectionError("The DaVinci Resolve app is not running.")
 
-    if resolve is None:
-        raise ConnectionError("The external application is not running.")
 
-    return resolve
+class TyResolveModuleError(Exception):
+    """
+    Raised when a error occured inside the TyResolve module.
+    """
+    def __init__(self, value, message):
+        self.value = value
+        self.message = message
+        super().__init__(f"{message}")
+
+
+DEBUG_ON = True
+
+
+def log_return_value(func):
+    """
+    デバッグモードが有効な場合のみ、関数名と戻り値をログ出力するデコレーター。
+    """
+    @functools.wraps(func)
+    def wrapper(*args, **kwargs):
+        result = func(*args, **kwargs)
+        if DEBUG_ON:
+            print(f"[Debug] {func.__name__}() -> {result}")
+        return result
+    return wrapper
 
 
 def get_project_manager():
     """
     Returns
     -------
-        ProjectManager (BlackmagicFusion.PyRemoteObject)
-            The project manager instance
+    ProjectManager (BlackmagicFusion.PyRemoteObject)
+        The project manager instance
 
     """
-    resolve = get_resolve()
     project_manager = resolve.GetProjectManager()
 
     return project_manager
 
 
+def _close_project(project):
+    """
+    Parameters
+    ----------
+    project : Project (BlackmagicFusion.PyRemoteObject)
+        A Project instance of the DaVinci Resolve
+
+    Returns
+    -------
+    Bool
+        Returns True if successful, and False otherwise.
+    """
+    project_manager = get_project_manager()
+    ret_val = project_manager.CloseProject(project)
+
+    return ret_val
+
+
+@log_return_value
+def close_current_project():
+    """
+    Returns
+    -------
+    Bool
+        Returns True if successful, and False otherwise.    
+    """
+    project_manager = get_project_manager()
+    project = project_manager.GetCurrentProject()
+    ret_val = _close_project(project=project)
+
+    return ret_val
+
+
+@log_return_value
+def delete_project(project_name):
+    """
+    Parameters
+    ----------
+    project_name : str
+        The project name
+
+    Returns
+    -------
+    Bool
+        Returns True if successful, and False otherwise.
+    """
+    project_manager = get_project_manager()
+    ret_val = project_manager.DeleteProject(project_name)
+
+    return ret_val
+
+
+@log_return_value
 def create_project(project_name):
     """
     Parameters
     ----------
-        project_name : str
-        A project name
+    project_name : str
+    A project name
 
     Returns
     -------
-        Project (BlackmagicFusion.PyRemoteObject)
-            If project does not already exist
-        None
-            If project is already exists.
+    Project (BlackmagicFusion.PyRemoteObject)
+        If project does not already exist
     """
     project_manager = get_project_manager()
-    ret_value = project_manager.CreateProject(project_name)
+    project = project_manager.CreateProject(project_name)
 
-    return ret_value
+    if project is None:
+        msg = f'"{project_name}" project is already exists. '
+        msg += 'Please use `load_project` function instead.'
+        raise TyResolveModuleError(project, msg)
+
+    return project
+
+
+@log_return_value
+def save_project():
+    """
+    Returns
+    -------
+    Bool
+        Returns True if successful.
+    """
+    project_manager = get_project_manager()
+    ret_val = project_manager.SaveProject()
+
+    if ret_val is None:
+        msg = 'Failed to save project. '
+        msg += 'Please save project manually.'
+        raise TyResolveModuleError(project, msg)
+
+    return ret_val
+
+
+@log_return_value
+def load_project(project_name):
+    """
+    Parameters
+    ----------
+    project_name : str
+    A project name
+
+    Returns
+    -------
+    Project (BlackmagicFusion.PyRemoteObject)
+        If project does not already exist
+    """
+    project_manager = get_project_manager()
+    project = project_manager.LoadProject(project_name)
+
+    if project is None:
+        msg = f'Failed to load "{project_name}" project. '
+        msg += 'Please verify that the `project_name` is correct.'
+        raise TyResolveModuleError(project, msg)
+
+    return project
 
 
 if __name__ == '__main__':
+    # sample code
     project_name = "Hello World3"
-    create_project(project_name=project_name)
-    # projectManager.LoadProject()
+
+    # control the project
+    close_current_project()
+    delete_project(project_name=project_name)
+    project = create_project(project_name=project_name)
+    save_project()
+    close_current_project()
+    project = load_project(project_name=project_name)
+
+    # set up the project settings
+    
+    # add clips
+
+    # create timelines
+
+    # encode
