3424cb7c0831430fe4bc44087615c59556853386
Toru Yoshihara <toru.ver.11@gmail.com>
2025-01-25 16:43:18 +0900

    add examples for blog

---
+++ 2024/16_Investigate_Resolve_API_Color_Space/blog_examples/dump_tool_id_and_value.py
@@ -0,0 +1,48 @@
+# -*- coding: utf-8 -*-
+import sys
+import os
+
+if sys.platform == "darwin":  # macOS
+    resolve_script_api = (
+        "/Library/Application Support/Blackmagic Design/DaVinci Resolve/"
+        "Developer/Scripting"
+    )
+    sys.path.append(os.path.join(resolve_script_api, "Modules"))
+    resolve_lut_path = \
+        "/Library/Application Support/Blackmagic Design/DaVinci Resolve/LUT/"
+elif sys.platform == "win32":  # Windows
+    resolve_script_api = os.path.expandvars(
+        r"%PROGRAMDATA%\Blackmagic Design\DaVinci Resolve\Support"
+        r"\Developer\Scripting"
+    )
+    sys.path.append(os.path.join(resolve_script_api, "Modules"))
+    resolve_lut_path = os.path.expandvars(
+        r"%PROGRAMDATA%\Blackmagic Design\DaVinci Resolve\Support\LUT"
+    )
+elif sys.platform == "linux":  # Linux
+    resolve_script_api = "/opt/resolve/Developer/Scripting"
+    sys.path.append(os.path.join(resolve_script_api, "Modules"))
+    resolve_lut_path = "/home/resolve/LUT"
+    ValueError("Linux platform is not supported")
+
+import DaVinciResolveScript as dvr_script
+
+resolve = dvr_script.scriptapp("Resolve")
+if resolve is None:
+    raise ConnectionError("The DaVinci Resolve app is not running.")
+
+# get fusion composition of the current project
+project = resolve.GetProjectManager().GetCurrentProject()
+timeline = project.GetCurrentTimeline()
+track_type = "video"
+track_idx = 1
+timeline_item_list = timeline.GetItemListInTrack(track_type, track_idx)
+timeline_item = timeline_item_list[0]
+fusion_comp = timeline_item.GetFusionCompByIndex(1)
+
+tool_name = "Background1"
+
+background = next(
+    (tool for tool in fusion_comp.GetToolList().values() if tool.Name == tool_name), None
+)
+print(background)
