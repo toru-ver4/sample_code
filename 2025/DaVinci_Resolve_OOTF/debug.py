e32f55f7f4573770359ae092159be5a07663f3d2
Toru Yoshihara <toru.ver.11@gmail.com>
2025-10-25 09:54:43 +0900

    #284 progress report

--- 2025/DaVinci_Resolve_OOTF/debug.py
+++ 2025/DaVinci_Resolve_OOTF/debug.py
@@ -1,10 +1,8 @@
 import os
 
 import numpy as np
-from colour.models import oetf_BT709, oetf_inverse_BT709
-
+from colour.models import oetf_BT709, oetf_inverse_BT709, eotf_BT1886, eotf_inverse_BT1886
 import test_pattern_generator2 as tpg
-import transfer_functions as tf
 import plot_utility as pu
 
 
@@ -18,17 +16,17 @@ def debug1_plot_three_graph(output_gamma: float | str = 2.4):
     inverse_ootf_fname = f"./img/Rec.709_Gamma{output_gamma_str}_Inverse-OOTF.png"
 
     fname_list = [no_ootf_fname, forward_ootf_fname, inverse_ootf_fname]
-    label_list = ["No OOTF", "Foward OOTF", "Inverse OOTF"]
-    color_list = [pu.RED, pu.GREEN, pu.BLUE]
+    label_list = ["Resolve: No OOTF", "Resolve: Foward OOTF", "Resolve: Inverse OOTF"]
+    color_list = [pu.GREEN, pu.RED, pu.BLUE]
 
 
     fig, ax1 = pu.plot_1_graph(
         fontsize=20,
         figsize=(10, 8),
         bg_color=(0.96, 0.96, 0.96),
-        graph_title=f"OOTF Analysis (Gamma = {output_gamma_str})",
+        graph_title=f"OOTF Characteristics (Gamma = {output_gamma_str})",
         graph_title_size=None,
-        xlabel=f"Input Code Value (Gamma = {output_gamma_str})",
+        xlabel=f"Timeline Code Value (Gamma = {output_gamma_str})",
         ylabel=f"Output Code Value (Gamma = {output_gamma_str})",
         axis_label_size=None,
         legend_size=17,
@@ -48,13 +46,17 @@ def debug1_plot_three_graph(output_gamma: float | str = 2.4):
         line_data = img[0, :, 1].reshape(-1)
         x = np.linspace(0, 1, len(line_data))
         ax1.plot(x, line_data, color=color, label=label)
-    x = np.linspace(0, 1, 1024)
-    bt1886_gamma = 2.4
-    e = gamma_func(x, gamma=output_gamma)
-    forward_simulation = gamma_inv_func((oetf_BT709(e) ** bt1886_gamma), gamma=output_gamma)
-    inverse_simulation = gamma_inv_func(oetf_inverse_BT709((e ** (1/bt1886_gamma))), gamma=output_gamma)
-    ax1.plot(x, inverse_simulation, '--', color=pu.PINK, lw=2, label="Inverse Simulation")
-    ax1.plot(x, forward_simulation, '--', color=pu.BROWN, lw=2, label="Forward Simulation")
+
+    timeline_ramp = np.linspace(0, 1, 1024)
+
+    e = non_linear_to_linear(timeline_ramp, gamma=output_gamma)
+    forward_simulation = eotf_BT1886(oetf_BT709(e))
+    inverse_simulation = oetf_inverse_BT709((eotf_inverse_BT1886(e)))
+    
+    forward_simulation_non_linear = linear_to_non_linear(forward_simulation, gamma=output_gamma)
+    inverse_simulation_non_linear = linear_to_non_linear(inverse_simulation, gamma=output_gamma)
+    ax1.plot(x, forward_simulation_non_linear, ':', color=pu.MAJENTA, lw=2, label="Python: Forward Simulation")
+    ax1.plot(x, inverse_simulation_non_linear, ':', color=pu.SKY, lw=2, label="Python: Inverse Simulation")
     graph_fname = f"./img/all_graph_gamma_{output_gamma_str}.png"
     print(graph_fname)
     pu.show_and_save(
@@ -62,48 +64,18 @@ def debug1_plot_three_graph(output_gamma: float | str = 2.4):
     )
 
 
-def gamma_inv_func(x, gamma: float | str = 2.4):
+def linear_to_non_linear(x, gamma: float | str = 2.4):
     if isinstance(gamma, str):
         return oetf_BT709(x)
     else:
         return x ** (1/gamma)
     
 
-def gamma_func(x, gamma: float | str = 2.4):
+def non_linear_to_linear(x, gamma: float | str = 2.4):
     if isinstance(gamma, str):
         return oetf_inverse_BT709(x)
     else:
         return x ** (gamma)
-
-
-def maybe_forward_ootf_simulation():
-    linear_light = np.linspace(0, 1, 2 ** 16)
-    y = oetf_BT709(linear_light)
-
-    fig, ax1 = pu.plot_1_graph(
-        fontsize=20,
-        figsize=(10, 8),
-        bg_color=(0.96, 0.96, 0.96),
-        graph_title="Title",
-        graph_title_size=None,
-        xlabel="X Axis Label",
-        ylabel="Y Axis Label",
-        axis_label_size=None,
-        legend_size=17,
-        xlim=None,
-        ylim=None,
-        xtick=None,
-        ytick=None,
-        xtick_size=None, ytick_size=None,
-        linewidth=3,
-        minor_xtick_num=None,
-        minor_ytick_num=None)
-    ax1.plot(linear_light, linear_light, label="Reference")
-    ax1.plot(linear_light, y, label="Rec.709 OETF")
-    ax1.plot(linear_light, y ** 2.4, label="Rec.709 OETF + Rec.1886 EOTF")
-    graph_fname = None
-    pu.show_and_save(
-        fig=fig, legend_loc='upper left', save_fname=graph_fname, show=True)
 
 
 if __name__ == '__main__':
@@ -111,4 +83,3 @@ if __name__ == '__main__':
     debug1_plot_three_graph(output_gamma=2.4)
     debug1_plot_three_graph(output_gamma=2.2)
     debug1_plot_three_graph(output_gamma="Rec.709")
-    # maybe_forward_ootf_simulation()
