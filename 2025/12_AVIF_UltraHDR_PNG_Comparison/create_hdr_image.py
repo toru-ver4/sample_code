9e66b6b04a288dfce30aacf3a72c6772c2868a00
toru.yoshihara <dummy_address@gmail.com>
2026-01-24 00:09:55 +0000

    #290 progress report

--- 2025/12_AVIF_UltraHDR_PNG_Comparison/create_hdr_image.py
+++ 2025/12_AVIF_UltraHDR_PNG_Comparison/create_hdr_image.py
@@ -1,6 +1,5 @@
 from pathlib import Path
 import os
-import sys
 import subprocess
 
 import numpy as np
@@ -11,15 +10,12 @@ from OpenImageIO import (
 )
 from colour.io import read_image
 
-tp_module_path = os.path.abspath(os.path.join(os.path.dirname(os.path.abspath(__file__)), "../08_UltraHDR_part2/"))
-sys.path.insert(0, tp_module_path)
-
 
 def png_to_avif(
         png_fname: str,
         avif_fname: str,
         bit_depth: int = 10,
-        cicp: list[int] = [1, 1, 0],
+        cicp: list[int] = [1, 1, 1],
         cll: int = 0,
         pall: int = 0
 ):
@@ -27,7 +23,7 @@ def png_to_avif(
         "avifenc", png_fname,
         "-d", f"{bit_depth}",
         "--cicp", f"{cicp[0]}/{cicp[1]}/{cicp[2]}",
-        "-r", "full",
+        "-c", "aom",
         "--clli", f"{cll},{pall}",
         "--lossless",
         "--ignore-exif",
@@ -37,36 +33,44 @@ def png_to_avif(
     subprocess.run(cmd)
 
 
-def save_png_using_oiio(img: np.ndarray, output_fname: str, cicp: list[int] | None = None):
-    output = ImageOutput.create(filename=output_fname)
-    yres, xres, channels = img.shape
-    image_spec = ImageSpec(xres, yres, channels, UINT16)
-    if cicp is not None:
-        image_spec.attribute("CICP", "int[4]", cicp)
-    output.open(filename=output_fname, spec=image_spec)
-    output.write_image(img)
-    output.close()
+def extract_obu_from_avif_using_ffmpeg(avif_fname):
+    output_fname = str(Path(avif_fname).with_suffix(".obu"))
+    cmd = [
+        "ffmpeg", "-hide_banner",
+        "-i", avif_fname,
+        "-map", "0:v:0",
+        "-c", "copy",
+        "-f", "obu",
+        output_fname,
+        '-y'
+    ]
+    print(" ".join(cmd))
+    subprocess.run(cmd)
 
 
 def main():
     src_hdr_img_fname = "./src_img/1920x1080_ST2084_Rec.2020.png"
-    src_sdr_img_fname = "./src_img/ultrahdr_sdr_image.png"
+    mdcv_luminance_param_list = [0, 10000]
+    cll_pall_param_list = [0, 10000]
 
     # # AVIF, PNG
     # dst_png_fname = "./dst_img/ISO_HDR_PNG_vs_AVIF_UltraHDR.png"
     # img = read_image(path="./src_img/1920x1080_ST2084_Rec.2020.png")
     # save_png_using_oiio(img=img, output_fname=dst_png_fname, cicp=[9, 16, 0, 1])
 
-    # AVIF
-    dst_avif_fname = "./dst_img/ISO_HDR_AVIF_vs_UltraHDR_PNG_ll-10000.avif"
-    png_to_avif(
-        png_fname=src_hdr_img_fname,
-        avif_fname=dst_avif_fname,
-        bit_depth=10,
-        cicp=[9, 16, 0, 1],
-        cll=10000,
-        pall=10000
-    )
+    for cll_pall_param in cll_pall_param_list:
+        # AVIF
+        dst_avif_fname = f"./dst_img/ISO_HDR_AVIF_MDCV-None_CLLI-{cll_pall_param}.avif"
+        png_to_avif(
+            png_fname=src_hdr_img_fname,
+            avif_fname=dst_avif_fname,
+            bit_depth=10,
+            cicp=[9, 16, 0],
+            cll=cll_pall_param,
+            pall=cll_pall_param
+        )
+
+    extract_obu_from_avif_using_ffmpeg(avif_fname=dst_avif_fname)
 
 
 if __name__ == '__main__':
--- 2025/12_AVIF_UltraHDR_PNG_Comparison/create_hdr_video.py
+++ 2025/12_AVIF_UltraHDR_PNG_Comparison/create_hdr_video.py
@@ -1,6 +1,5 @@
 from pathlib import Path
 import os
-import sys
 import subprocess
 
 import numpy as np
